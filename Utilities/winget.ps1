1429d40be586324eb525463bac8a72f46381975d
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-11-28 21:02:49 +0700

    Update winget.ps1

--- Utilities/winget.ps1
+++ Utilities/winget.ps1
@@ -18,11 +18,13 @@ Set-Location $env:temp\temp
 $path = "$env:temp\temp"
 
 #Install C++ Runtime framework packages for Desktop Bridge
+$ProgressPreference='Silent'
 $url = 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
 (New-Object Net.WebClient).DownloadFile($url, "$env:temp\temp\Microsoft.VCLibs.x64.14.00.Desktop.appx")
 Add-AppxPackage -Path Microsoft.VCLibs.x64.14.00.Desktop.appx -ErrorAction SilentlyContinue | Out-Null
 
 #Download and extract Nuget
+$ProgressPreference='Silent'
 $url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 (New-Object Net.WebClient).DownloadFile($url, "$env:temp\temp\nuget.exe")
 .\nuget.exe install Microsoft.UI.Xaml -Version 2.7 | Out-Null
