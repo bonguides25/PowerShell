976a3273e87ac1cac6c8b122c5421bf3ef732d93
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-11-28 20:50:41 +0700

    Update winget.ps1

--- Utilities/winget.ps1
+++ Utilities/winget.ps1
@@ -22,18 +22,15 @@ $ProgressPreference='Silent'
 $url = 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
 (New-Object Net.WebClient).DownloadFile($url, "$env:temp\temp\Microsoft.VCLibs.x64.14.00.Desktop.appx")
 Add-AppxPackage -Path Microsoft.VCLibs.x64.14.00.Desktop.appx -ErrorAction SilentlyContinue | Out-Null
+
 #Download and extract Nuget
-Write-Host
-Write-Host Installing Nuget...
-$ProgressPreference='Silent'
 $url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 (New-Object Net.WebClient).DownloadFile($url, "$env:temp\temp\nuget.exe")
 .\nuget.exe install Microsoft.UI.Xaml -Version 2.7 | Out-Null
 Add-AppxPackage -Path "$path\Microsoft.UI.Xaml.2.7.0\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx" -ErrorAction:SilentlyContinue | Out-Null
 
 #Download winget and license file
-Write-Host
-Write-Host Installing Windows Package Manager...
+Write-Host '`nInstalling Windows Package Manager...'
 function getLink($match) {
     $uri = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
     $get = Invoke-RestMethod -uri $uri -Method Get -ErrorAction stop
@@ -52,11 +49,9 @@ $licenseName = 'license1.xml'
 (New-Object Net.WebClient).DownloadFile($licenseUrl, "$env:temp\temp\$licenseName")
 
 Add-AppxProvisionedPackage -Online -PackagePath $fileName -LicensePath $licenseName | Out-Null
-Write-Host
-Write-Host Installed packages:
-Write-Host
-# Checking installed apps
 
+# Checking installed apps
+Write-Host '`nInstalled packages:'
 $packages = @("DesktopAppInstaller")
 $report = ForEach ($package in $packages){Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -like "*$package*"} | select DisplayName,Version}
 $report | format-table
