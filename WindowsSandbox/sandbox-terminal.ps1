7137aaa5a23fdc47051ca3a3f4561653b1e19ddc
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-11-29 18:46:14 +0700

    Update sandbox-terminal.ps1

--- WindowsSandbox/sandbox-terminal.ps1
+++ WindowsSandbox/sandbox-terminal.ps1
@@ -1,17 +1,16 @@
 #Install and update Desktop framework packages
+$progressPreference = 'silentlyContinue'
 Set-Location $env:temp
-ii $env:temp
-$uri = 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
-(New-Object Net.WebClient).DownloadFile($uri, "$env:temp\Microsoft.VCLibs.x64.14.00.Desktop.appx")
-Add-AppxPackage -Path .\Microsoft.VCLibs.x64.14.00.Desktop.appx
+Write-Information "Downloading dependencies..."
+Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+Invoke-WebRequest -Uri 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx' -OutFile 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
+Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx' -OutFile 'Microsoft.UI.Xaml.2.7.x64.appx'
 
-$ProgressPreference='Silent'
-$url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-(New-Object Net.WebClient).DownloadFile($url, "$env:temp\nuget.exe")
-.\nuget.exe install Microsoft.UI.Xaml -Version 2.8 | Out-Null
-Add-AppxPackage -Path ".\Microsoft.UI.Xaml.2.8.0\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.8.appx" -ErrorAction:SilentlyContinue | Out-Null
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 
-#Download the package to the Downloads folder of current logged on user
+#Download and install Windows Terminal
 $url = 'https://github.com/microsoft/terminal/releases/latest'
 $request = [System.Net.WebRequest]::Create($url)
 $response = $request.GetResponse()
@@ -23,13 +22,3 @@ $downloadUrl = $tagUrl.Replace('tag', 'download') + '/' + $fileName
 
 #Install Windows Terminal
 Add-AppxPackage -Path ".\WindowsTerminal.msixbundle"
-
-
-$progressPreference = 'silentlyContinue'
-Write-Information "Downloading WinGet and its dependencies..."
-Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
-Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
-Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
