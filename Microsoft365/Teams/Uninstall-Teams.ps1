90161cf89f9f2f90aadd283ac017f5d11eed3825
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-11-29 07:01:04 +0700

    Update Uninstall-Teams.ps1

--- Microsoft365/Teams/Uninstall-Teams.ps1
+++ Microsoft365/Teams/Uninstall-Teams.ps1
@@ -12,7 +12,7 @@ if ($clearCache -eq "Y"){
     Write-Host "Stopping Teams Process" -ForegroundColor Yellow
 
     try{
-        Get-Process -ProcessName Teams | Stop-Process -Force
+        Get-Process | where {$_.ProcessName -like '*teams*'} | Stop-Process -Force -ErrorAction SilentlyContinue
         Start-Sleep -Seconds 3
         Write-Host "Teams Process Sucessfully Stopped" -ForegroundColor Green
     }catch{
@@ -22,14 +22,14 @@ if ($clearCache -eq "Y"){
     Write-Host "Clearing Teams Disk Cache" -ForegroundColor Yellow
 
     try{
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\application cache\cache" | Remove-Item -Confirm:$false
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\blob_storage" | Remove-Item -Confirm:$false
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\databases" | Remove-Item -Confirm:$false
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\cache" | Remove-Item -Confirm:$false
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\gpucache" | Remove-Item -Confirm:$false
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\Indexeddb" | Remove-Item -Confirm:$false
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\Local Storage" | Remove-Item -Confirm:$false
-        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\tmp" | Remove-Item -Confirm:$false
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\application cache\cache" -Recurse -ErrorAction SilentlyContinue| Remove-Item -Confirm:$false -Recurse
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\blob_storage" -Recurse -ErrorAction SilentlyContinue| Remove-Item -Confirm:$false -Recurse
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\databases" -Recurse -ErrorAction SilentlyContinue| Remove-Item -Confirm:$false -Recurse
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\cache" -Recurse -ErrorAction SilentlyContinue| Remove-Item -Confirm:$false -Recurse
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\gpucache" -Recurse -ErrorAction SilentlyContinue| Remove-Item -Confirm:$false -Recurse
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\Indexeddb" -Recurse -ErrorAction SilentlyContinue| Remove-Item -Confirm:$false -Recurse
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\Local Storage" -Recurse -ErrorAction SilentlyContinue| Remove-Item -Confirm:$false -Recurse
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\tmp" -Recurse -ErrorAction SilentlyContinue| Remove-Item -Confirm:$false -Recurse
         Write-Host "Teams Disk Cache Cleaned" -ForegroundColor Green
     }catch{
         Write-Output $_
@@ -37,9 +37,39 @@ if ($clearCache -eq "Y"){
 }
 
 if ($uninstall -eq "Y"){
-    Write-Host "Removing Teams apps..." -ForegroundColor Yellow
+
+    Write-Host "Removing Teams Machine-wide Installer" -ForegroundColor Yellow
+    $MachineWide = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Teams Machine-Wide Installer"}
+    $MachineWide.Uninstall()
+
+    function unInstallTeams($path) {
+        $clientInstaller = "$($path)\Update.exe"
+        try {
+            $process = Start-Process -FilePath "$clientInstaller" -ArgumentList "--uninstall /s" -PassThru -Wait -ErrorAction STOP
+            if ($process.ExitCode -ne 0) {
+                Write-Error "UnInstallation failed with exit code  $($process.ExitCode)."
+            }
+        }
+        catch {
+            Write-Error $_.Exception.Message
+        }
+    }
+    
+    #Locate installation folder
+    $localAppData = "$($env:LOCALAPPDATA)\Microsoft\Teams"
+    $programData = "$($env:ProgramData)\$($env:USERNAME)\Microsoft\Teams"
+    
+    If (Test-Path "$($localAppData)\Current\Teams.exe") {
+        unInstallTeams($localAppData)
+    } elseif (Test-Path "$($programData)\Current\Teams.exe") {
+        unInstallTeams($programData)
+    } else {
+        Write-Warning  "Teams installation not found"
+    }
+
     $winget = Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq 'Microsoft.DesktopAppInstaller'}
     If ([Version]$winGet.Version -lt "2022.506.16.0") {
+        Write-Host "Updating Windows Package Manager..." -ForegroundColor Yellow
         Invoke-RestMethod bonguides.com/winget | Invoke-Expression
     }
 
@@ -48,9 +78,7 @@ if ($uninstall -eq "Y"){
 
     If ($winget.count -gt 1){ $winget = $winget[-1] }
 
-    & $winget --uninstall Microsoft.Teams --exact --silent --force -ErrorAction SilentlyContinue
-    & $winget --uninstall Microsoft.Teams.Classic --exact --silent --force -ErrorAction SilentlyContinue
-    & $winget --uninstall MicrosoftTeams_8wekyb3d8bbwe --exact --silent --force -ErrorAction SilentlyContinue
-    & $winget --uninstall {731F6BAA-A986-45A4-8936-7C3AAAAA760B} --exact --silent --force -ErrorAction SilentlyContinue
-
+    Write-Host "Removing Teams apps..." -ForegroundColor Yellow
+    & $winget uninstall Microsoft.Teams --exact --silent --force
+    & $winget uninstall MicrosoftTeams_8wekyb3d8bbwe --exact --silent --force
 }
