13cb158a5a10dc6ac95df0751616c87a0e0b6d95
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-11-29 18:43:29 +0700

    1

--- WindowsSandbox/sandbox-terminal.ps1
+++ WindowsSandbox/sandbox-terminal.ps1
@@ -22,5 +22,14 @@ $downloadUrl = $tagUrl.Replace('tag', 'download') + '/' + $fileName
 (New-Object Net.WebClient).DownloadFile($downloadUrl, "$env:temp\WindowsTerminal.msixbundle")
 
 #Install Windows Terminal
-$path = Get-ChildItem -Name "*.msixbundle"
 Add-AppxPackage -Path ".\WindowsTerminal.msixbundle"
+
+
+$progressPreference = 'silentlyContinue'
+Write-Information "Downloading WinGet and its dependencies..."
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
