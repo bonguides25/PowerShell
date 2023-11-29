fd4730581a486b3dc507ebb7f0ea35b610be9492
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-11-29 17:05:06 +0700

    Update sandbox-terminal.ps1

--- WindowsSandbox/sandbox-terminal.ps1
+++ WindowsSandbox/sandbox-terminal.ps1
@@ -4,7 +4,11 @@ $uri = 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
 (New-Object Net.WebClient).DownloadFile($uri, "$env:temp\Microsoft.VCLibs.x64.14.00.Desktop.appx")
 Add-AppxPackage -Path 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
 
-
+$ProgressPreference='Silent'
+$url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+(New-Object Net.WebClient).DownloadFile($url, "$env:temp\nuget.exe")
+.\nuget.exe install Microsoft.UI.Xaml -Version 2.7 | Out-Null
+Add-AppxPackage -Path ".\Microsoft.UI.Xaml.2.7.0\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx" -ErrorAction:SilentlyContinue | Out-Null
 
 #Download the package to the Downloads folder of current logged on user
 $url = 'https://github.com/microsoft/terminal/releases/latest'
@@ -14,8 +18,8 @@ $tagUrl = $response.ResponseUri.OriginalString
 $version = $tagUrl.split('/')[-1].Trim('v')
 $fileName = "Microsoft.WindowsTerminal_$($version)_8wekyb3d8bbwe.msixbundle"
 $downloadUrl = $tagUrl.Replace('tag', 'download') + '/' + $fileName
-(New-Object Net.WebClient).DownloadFile($downloadUrl, "$($env:temp)/$($fileName)")
+(New-Object Net.WebClient).DownloadFile($downloadUrl, "$env:temp\WindowsTerminal.msixbundle")
 
 #Install Windows Terminal
-$path = Get-ChildItem -Name "*Microsoft.WindowsTerminal*"
+$path = Get-ChildItem -Name "*.msixbundle"
 Add-AppxPackage -Path $path
