53e1fbdf5f18efd7a3b1f33cb5dcad80e5c44afd
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-01 21:13:45 +0700

    Update sandbox-store.ps1

--- WindowsSandbox/sandbox-store.ps1
+++ WindowsSandbox/sandbox-store.ps1
@@ -1,5 +1,8 @@
-
-
+if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
+    # Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm bonguides.com/wsb/msstore.com | iex"
+    break
+}
 
 if ([System.Environment]::OSVersion.Version.Build -lt 16299) {
     Write-Host "This pack is for Windows 10 version 1709 and later"
