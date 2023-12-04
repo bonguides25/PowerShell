415ba431e85b382a79087678471330d5ed8c6878
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-04 21:37:35 +0700

    Update sandbox-winget.ps1

--- WindowsSandbox/sandbox-winget.ps1
+++ WindowsSandbox/sandbox-winget.ps1
@@ -1,6 +1,12 @@
+if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
+    # Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm bonguides.com/wsb/msstore.com | iex"
+    break
+}
+
 Set-Location $env:temp
 $progressPreference = 'silentlyContinue'
-Write-Information "Downloading WinGet and its dependencies..."
+Write-Host "`nInstalling WinGet and its dependencies..."
 Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
 Invoke-WebRequest -Uri 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx' -OutFile 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
 Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx' -OutFile 'Microsoft.UI.Xaml.2.7.x64.appx'
