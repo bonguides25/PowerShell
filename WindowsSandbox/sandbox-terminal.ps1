8d605124ed82019d8a36e010162084c9d0de08a7
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-04 21:23:19 +0700

    Update sandbox-terminal.ps1

--- WindowsSandbox/sandbox-terminal.ps1
+++ WindowsSandbox/sandbox-terminal.ps1
@@ -13,8 +13,6 @@ Invoke-WebRequest -Uri 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx' 
 Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx' -OutFile 'Microsoft.UI.Xaml.2.7.x64.appx'
 Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.5/Microsoft.UI.Xaml.2.8.x64.appx' -OutFile 'Microsoft.UI.Xaml.2.8.x64.appx'
 
-
-
 Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
 Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
 Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
