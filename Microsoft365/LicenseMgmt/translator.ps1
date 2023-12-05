3f6baede20ef07a941f783aca8a4ae1c6f9ed7a5
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-05 16:03:54 +0700

    Create translator.ps1

---
+++ Microsoft365/LicenseMgmt/translator.ps1
@@ -0,0 +1,18 @@
+$skus = Get-MgSubscribedSku -All
+$translationTable = Invoke-RestMethod -Method GET -Uri "https://download.microsoft.com/download/e/3/e/e3e9faf2-f28b-490a-9ada-c6089a1fc5b0/Product%20names%20and%20service%20plan%20identifiers%20for%20licensing.csv" | ConvertFrom-Csv
+
+$output = @()
+foreach ($sku in $skus) {
+    $skuNamePretty = ($translationTable | Where-Object {$_.GUID -eq $sku.skuId} | Sort-Object Product_Display_Name -Unique).Product_Display_Name
+    $skuDetails = [PSCustomObject][Ordered]@{
+        LicenseName   = $skuNamePretty
+        SkuPartNumber = $Sku.SkuPartNumber
+        SkuId         = $Sku.SkuId
+        ActiveUnits   = $Sku.PrepaidUnits.Enabled
+        ConsumedUnits = $Sku.ConsumedUnits
+
+    }
+    $output += $skuDetails
+}
+
+$output
