9d8ee8a07eee5df9e55244f0b7252aff23e8f9cf
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-10 02:27:50 +0700

    Update ModulesInstallation.ps1

--- Microsoft365/ExchangeOnline/ModulesInstallation.ps1
+++ Microsoft365/ExchangeOnline/ModulesInstallation.ps1
@@ -1,51 +1,64 @@
+<#=============================================================================================
+Script by    : Leo Nguyen
+Website      : www.bonguides.com
+Telegram     : https://t.me/bonguides
+Discord      : https://discord.gg/fUVjuqexJg
+YouTube      : https://www.youtube.com/@BonGuides
+Description  : Install Exchange Online PowerShell modules
+============================================================================================#>
+
 # Required running with elevated right.
-if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
-    break
- }
+    if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+        Write-Warning "`nYou need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
+        # Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm  | iex"
+        break
+    }
 
-# Configure Execution Policy
-if ((Get-ExecutionPolicy) -notmatch "RemoteSigned") {
-    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine -Force
-}
+# Configure the system before install the module
+    Function InstallDeps {
 
-# Update the PowerShellGet if needed.
-$PSGetCurrentVersion = (Get-PackageProvider -Name 'PowerShellGet').Version
-$PSGetLatestVersion = (Find-Module PowerShellGet).Version
-if ($PSGetCurrentVersion -lt $PSGetLatestVersion) {
-    Write-Host "`nUpdating PowerShellGet Module from $PSGetCurrentVersion to $PSGetLatestVersion..."
-    Install-Module -Name 'PowerShellGet' -Force
-}
+        # Configure Execution Policy
+            Set-ExecutionPolicy Bypass -Scope Process -Force | Out-Null
 
-# We're installing from the PowerShell Gallery so make sure that it's trusted.
-$InstallationPolicy = (Get-PSRepository -Name PSGallery).InstallationPolicy
-if ($InstallationPolicy -match "Untrusted") {
-   Write-host "`nConfiguring the PowerShell Gallery Repository..."
-   Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-}
+        # Update the NuGet Provider if needed.
+            $nuGetPath = "C:\Program Files\PackageManagement\ProviderAssemblies\nuget\*\Microsoft.PackageManagement.NuGetProvider.dll"
+            $testPath = Test-Path -Path $nuGetPath
+            if ($testPath -match 'false') {
+                Write-Host "Installing NuGet Provider..." -ForegroundColor Yellow
+                Install-PackageProvider -Name NuGet -Force | Out-Null
+            }
 
-# Update the NuGet Provider if needed.
-$nuGetPath = "C:\Program Files\PackageManagement\ProviderAssemblies\nuget\*\Microsoft.PackageManagement.NuGetProvider.dll"
-$testPath = Test-Path -Path $nuGetPath
-if ($testPath -match 'false') {
-    Write-Host "`nInstalling NuGet Provider..."
-    Install-PackageProvider -Name NuGet -Force | Out-Null
-}
+        # Update the PowerShellGet if needed.
+            $PSGetCurrentVersion = (Get-PackageProvider -Name 'PowerShellGet').Version
+            $PSGetLatestVersion = (Find-Module PowerShellGet).Version
+            if ($PSGetCurrentVersion -lt $PSGetLatestVersion) {
+                Write-Host "Updating PowerShellGet Module from $PSGetCurrentVersion to $PSGetLatestVersion..." -ForegroundColor Yellow
+                Install-Module -Name 'PowerShellGet' -Force
+            }
 
-$ExchangeOnlineModule =  Get-Module ExchangeOnlineManagement -ListAvailable
-if($null -eq $ExchangeOnlineModule)
-{ 
-    Write-host "Important: Exchange Online module is unavailable. It is mandatory to have this module installed in the system to run the script successfully." 
-    $confirm = Read-Host Are you sure you want to install Exchange Online module? [Y] Yes [N] No  
-    if($confirm -match "[yY]") 
+        # We're installing from the PowerShell Gallery so make sure that it's trusted.
+            $InstallationPolicy = (Get-PSRepository -Name PSGallery).InstallationPolicy
+            if ($InstallationPolicy -match "Untrusted") {
+                Write-host "Configuring the PowerShell Gallery Repository..." -ForegroundColor Yellow
+                Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+            }
+    }
+
+# Install the PowerShell module
+    $exoModule =  Get-Module ExchangeOnlineManagement -ListAvailable
+    if($null -eq $exoModule)
     { 
-        Write-host "Installing Exchange Online module..."
-        Install-Module -Name ExchangeOnlineManagement -Scope CurrentUser
-        Write-host "Exchange Online Module is installed in the machine successfully" -ForegroundColor Magenta 
-    } 
-    else
-    { 
-        Write-host "Exiting. `nNote: Exchange Online module must be available in your system to run the script" 
-        Exit 
-    } 
+        Write-host "Important: Exchange Online module is unavailable. `nIt is mandatory to have this module installed in the system to run the script successfully." -ForegroundColor Yellow
+        $confirm = Read-Host Are you sure you want to install Exchange Online module? [Y] Yes [N] No  
+        if($confirm -match "[yY]") 
+        { 
+            Write-host "Installing Exchange Online module..." -ForegroundColor Yellow
+            Install-Module -Name ExchangeOnlineManagement -Scope CurrentUser
+            Write-host "Exchange Online Module is installed in the machine successfully" -ForegroundColor Magenta 
+        } 
+        else
+        { 
+            Write-host "Exiting. `nNote: Exchange Online module must be available in your system to run the script" 
+            Exit 
+        } 
 }
