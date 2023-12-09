ef20cce4d9cde630c72e34d7c92cb588241874e5
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-09 09:30:31 +0700

    Update template.ps1

--- template.ps1
+++ template.ps1
@@ -1,5 +1,19 @@
+<#=============================================================================================
+Script by    : Leo Nguyen
+Website      : www.bonguides.com
+Telegram     : https://t.me/bonguides
+Discord      : https://discord.gg/fUVjuqexJg
+YouTube      : https://www.youtube.com/@BonGuides
+Description  : Export Microsoft 365 users' last logon time report using PowerShell
+
+Script Highlights:
+~~~~~~~~~~~~~~~~~
+#. Single script allows you to generate last login reports.
+============================================================================================#>
+
+
 if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
-    # Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm msgang.com/dl | iex"
+    # Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm  | iex"
     break
 }
