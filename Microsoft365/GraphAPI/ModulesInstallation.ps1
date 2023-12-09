c0fd4ac205f16b963ae6c976c84a4bcccf8d70f3
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-09 09:52:18 +0700

    Update ModulesInstallation.ps1

--- Microsoft365/GraphAPI/ModulesInstallation.ps1
+++ Microsoft365/GraphAPI/ModulesInstallation.ps1
@@ -11,19 +11,19 @@ Script Highlights:
 #. Microsoft Graph PowerShell SDK detection then install.
 ============================================================================================#>
 
-# Required running with elevated right.
-if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Warning "`nYou need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
-    # Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm  | iex"
-    break
-}
-
 Param (
     [switch]$InstallMainBasic,
     [switch]$InstallMainAll,
     [switch]$InstallBetaBasic,
     [switch]$InstallBetaAll
 )
+
+# Required running with elevated right.
+if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "`nYou need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
+    # Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm  | iex"
+    break
+}
 
 Function InstallDeps {
 
