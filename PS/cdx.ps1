ab38f14276d439412c54aac6d9acaae7eacb6ae1
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-10 15:28:57 +0700

    Create cdx.ps1

---
+++ PS/cdx.ps1
@@ -0,0 +1,57 @@
+
+
+Disconnect-Graph
+Start-Sleep -Seconds 1
+Disconnect-Graph
+
+
+$scopes = @('Directory.ReadWrite.All','User.ReadWrite.All')
+Connect-MgGraph -Scopes $scopes
+
+# Creating users in bulk
+
+    $domain = Get-MgDomain | select -ExpandProperty Id
+    $items = @(1..6)
+    foreach ($item in $items) {
+        $params = @{
+            AccountEnabled = $true
+            DisplayName = "Account$item"
+            UserPrincipalName = "account$item@$domain"
+            MailNickname = "account$item"
+            UsageLocation = 'US'
+            PasswordProfile = @{
+                ForceChangePasswordNextSignIn = $false
+                Password = 'Nttg$ti74fnff[gr4]'
+            }
+        }
+        Write-Progress -Activity "Creating $($params.UserPrincipalName)" -Status "Created: $item of $($items.Count)"
+        New-MgUser -BodyParameter $params
+    }
+
+Write-Host "Assign licenses and add members to group." -ForegroundColor Green
+$users = Get-MgUser -ConsistencyLevel eventual -Count userCount -Filter "startsWith(DisplayName, 'Account')" -OrderBy UserPrincipalName
+$groupId = (Get-MgGroup -ConsistencyLevel eventual -Count groupCount -Search '"DisplayName:sg-CloudPCUsers"').Id
+$sku1 = (Get-MgSubscribedSku | Where-Object {$_.SkuPartNumber -match 'CPC_E_2C_8GB_256GB'}).SkuId
+$sku2 = (Get-MgSubscribedSku | Where-Object {$_.SkuPartNumber -match 'CPC_E_2C_4GB_128GB​'}).SkuId
+
+while ($users.Count -lt 6) {
+    Start-Sleep 1
+}
+
+foreach ($user in $users) {
+    Set-MgUserLicense -UserId $($user.Id) -Addlicenses @{SkuId = $sku1} -RemoveLicenses @()
+    Set-MgUserLicense -UserId $($user.Id) -Addlicenses @{SkuId = $sku2} -RemoveLicenses @()
+    New-MgGroupMember -GroupId $groupId -DirectoryObjectId $($user.Id)
+}
+
+Write-Host "List of members:" -ForegroundColor Green
+Get-MgGroupMember -GroupId $groupId | select AdditionalProperties
+
+Write-Host "`nDone." -ForegroundColor Green
+
+Disconnect-Graph
+
+
+
+
+
