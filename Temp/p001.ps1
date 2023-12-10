98f8b5ed3cf8c17aebcce1378cabcc866d34d9b2
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-10 12:16:57 +0700

    Update p001.ps1

--- Temp/p001.ps1
+++ Temp/p001.ps1
@@ -5,8 +5,9 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocola
 $apps = @('telegram', 'firefox','winscp', 'zoom', 'vscode', 'github-desktop')
 choco feature enable -n allowGlobalConfirmation
 
+Write-Host
 $apps | ForEach-Object {
-    Write-Host "`nInstalling $_ using Chocolatey (choco)..." -ForegroundColor Green
+    Write-Host "Installing $_..." -ForegroundColor Green
     choco install $_ -y | Out-Null
 }
 
