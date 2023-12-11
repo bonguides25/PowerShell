2f9f33895df4f5ed1eca61f4018b7905b22e872d
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-12 06:19:00 +0700

    Update translator.ps1

--- Microsoft365/LicenseMgmt/translator.ps1
+++ Microsoft365/LicenseMgmt/translator.ps1
@@ -1,3 +1,15 @@
+<#=============================================================================================
+Script by    : Leo Nguyen
+Website      : www.bonguides.com
+Telegram     : https://t.me/bonguides
+Discord      : https://discord.gg/fUVjuqexJg
+YouTube      : https://www.youtube.com/@BonGuides
+============================================================================================#>
+param (
+    [switch]$OutCSV,
+    [switch]$OutGridView
+)
+
 $skus = Get-MgSubscribedSku -All
 $translationTable = Invoke-RestMethod -Method GET -Uri "https://download.microsoft.com/download/e/3/e/e3e9faf2-f28b-490a-9ada-c6089a1fc5b0/Product%20names%20and%20service%20plan%20identifiers%20for%20licensing.csv" | ConvertFrom-Csv
 
@@ -14,4 +26,17 @@ foreach ($sku in $skus) {
     }
     $output += $skuDetails
 }
-$output | Format-Table
+
+
+if($OutCSV.IsPresent) {
+    # $result | Sort-Object assignedlicenses -Descending 
+    # $result | Out-GridView
+    $filePath = "$env:userprofile\desktop\Result-$(Get-Date -Format yyyy-mm-dd-hh-mm-ss).csv"
+    $result | Export-CSV $filePath -NoTypeInformation -Encoding UTF8
+    Write-Host "The report is saved to: $filePath `n" -ForegroundColor Cyan
+    Invoke-Item "$env:userprofile\desktop"
+} elseif ($OutGridView.IsPresent) {
+    $output | Out-GridView
+} else {
+    $output | Format-Table
+}
