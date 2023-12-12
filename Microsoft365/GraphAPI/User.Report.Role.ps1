2f942f36ea91524c8bce612a10e17cdab93ad62a
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-12 08:39:40 +0700

    Update User.Report.Role.ps1

--- Microsoft365/GraphAPI/User.Report.Role.ps1
+++ Microsoft365/GraphAPI/User.Report.Role.ps1
@@ -7,8 +7,15 @@ YouTube      : https://www.youtube.com/@BonGuides
 
 Script Highlights:
 ~~~~~~~~~~~~~~~~~
-#. Single script allows you to generate user report with license assigments and account status
+#. Single script allows you to generate user report with roles assignments
 ============================================================================================#>
+
+param (
+    [switch]$InstallMainBasic,
+    [switch]$InstallMainAll,
+    [switch]$OutCSV,
+    [switch]$OutGridView
+)
 
 if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
@@ -18,88 +25,53 @@ if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdenti
 
 # Install the required Microsoft Graph PowerShell SDK modules
     Set-ExecutionPolicy Bypass -Scope Process -Force | Out-Null
-    iex "& { $(irm bonguides.com/graph/modulesinstall) } -InstallBetaBasic"
+    iex "& { $(irm bonguides.com/graph/modulesinstall) } -InstallMainBasic"
 
 # Get user report with license assigments and account status
-    $result = @()
-    $uri = "https://bonguides.com/files/LicenseFriendlyName.txt"
-    $friendlyNameHash = Invoke-RestMethod -Method GET -Uri $uri | ConvertFrom-StringData
 
     Disconnect-MgGraph -ErrorAction:SilentlyContinue | Out-Null
 
     Write-Host "Conncting to Microsoft Graph PowerShell..." -ForegroundColor Yellow
-    Connect-MgGraph -Scopes "Directory.Read.All" -ErrorAction Stop
-    $users  = Get-MgBetaUser -All
+    Connect-MgGraph -Scopes 'Directory.Read.All', 'User.Read.All' -ErrorAction Stop
+
+    $users  = Get-MgUser -All
 
     # Get licenses assigned to user accounts
     $i = 1
+    $Roles = @()
+    $result = @()
     foreach ($user in $users) {
+        #Get roles assigned to user
         Write-Host "($i/$($users.Count)) Processing: $($user.UserPrincipalName) - $($user.DisplayName)" -ForegroundColor Green
-        $licenses = (Get-MgBetaUserLicenseDetail -UserId $user.id).SkuPartNumber
-        $assignedLicense = @()
-        # Convert license plan to friendly name
-        if($licenses.count -eq 0){
-            $assignedLicense = "Unlicensed"
-        } else {
-            foreach($License in $licenses){
-                $EasyName = $friendlyNameHash[$License]
-                if(!($EasyName)){
-                    $NamePrint = $License
-                } else {
-                    $NamePrint = $EasyName
-                }
-                $assignedLicense += $NamePrint
-            }
-        }
+        $Roles = Get-MgBetaUserTransitiveMemberOf -UserId $user.Id | Select-Object -ExpandProperty AdditionalProperties
+        $Roles = $Roles | Where-Object{$_.'@odata.type' -eq '#microsoft.graph.directoryRole'} 
+        if($Roles.count -eq 0) { 
+            $RolesAssigned = "No roles" 
+        } else { 
+            $RolesAssigned = @($Roles.displayName) -join ',' 
+        } 
 
         # Creating the custom report
         $result += [PSCustomObject]@{
             'DisplayName' = $user.DisplayName
             'UserPrincipalName' = $user.UserPrincipalName
             'Enabled' = $user.accountEnabled
-            'Assignedlicenses'=(@($assignedLicense)-join ',')
+            'Roles' = $RolesAssigned
         }
         $i++
     }
 
-    Write-Host "`nDone. Generating report..." -ForegroundColor Yellow
-
-    # Output options to console, graphical grid view or export to CSV file.
-        # $result | Sort-Object assignedlicenses -Descending 
-        # $result | Out-GridView
-        $filePath = "$env:userprofile\desktop\Result-$(Get-Date -Format yyyy-mm-dd-hh-mm-ss).csv"
-        $result | Export-CSV $filePath -NoTypeInformation -Encoding UTF8
-        Write-Host "The report is saved to: $filePath `n" -ForegroundColor Cyan
-
+# Output options to console, graphical grid view or export to CSV file.
+if($OutCSV.IsPresent) {
+    # $result | Sort-Object assignedlicenses -Descending 
+    # $result | Out-GridView
+    $filePath = "$env:userprofile\desktop\Result-$(Get-Date -Format yyyy-mm-dd-hh-mm-ss).csv"
+    $result | Export-CSV $filePath -NoTypeInformation -Encoding UTF8
+    Write-Host "`nThe report is saved to: $filePath `n" -ForegroundColor Cyan
     Invoke-Item "$env:userprofile\desktop"
+} elseif ($OutGridView.IsPresent) {
+    $result | Out-GridView
+} else {
+    $result | Format-Table
+}
 
-Function ProcessMailBox
-{
-    #Get roles assigned to user
-    $Roles = @()
-    $Roles = Get-MgBetaUserTransitiveMemberOf -UserId $UPN |Select-Object -ExpandProperty AdditionalProperties
-    $Roles = $Roles | Where-Object{$_.'@odata.type' -eq '#microsoft.graph.directoryRole'} 
-    if($Roles.count -eq 0) 
-    { 
-        $RolesAssigned = "No roles" 
-    } 
-    else 
-    { 
-        $RolesAssigned = @($Roles.displayName) -join ',' 
-    } 
-
-
-    # Creating the custom report
-    $result += [PSCustomObject]@{
-        'DisplayName' = $user.DisplayName
-        'UserPrincipalName' = $user.UserPrincipalName
-        'Enabled' = $user.accountEnabled
-        'Roles' = $RolesAssigned
-        'Assignedlicenses'=(@($assignedLicense)-join ',')
-    }
-    $i++
-
-
-#Module functions
-ConnectModules
-CloseConnection
