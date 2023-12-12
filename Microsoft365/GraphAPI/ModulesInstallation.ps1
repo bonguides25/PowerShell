41ff024b072b356de4eb189be3b895bf4c501938
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-12 08:43:28 +0700

    Update ModulesInstallation.ps1

--- Microsoft365/GraphAPI/ModulesInstallation.ps1
+++ Microsoft365/GraphAPI/ModulesInstallation.ps1
@@ -52,7 +52,7 @@ Function InstallDeps {
 
 Function InstallMainAll {
 
-    $MsGraphModule =  Get-Module Microsoft.Graph -ListAvailable
+    $MsGraphModule =  Get-Module Microsoft.Graph.Authentication -ListAvailable
     if($null -eq $MsGraphModule) {
         Write-host "Important: Microsoft Graph module is unavailable. `nIt is mandatory to have this module installed in the system to run the script successfully." -ForegroundColor Yellow
         $confirm = Read-Host Are you sure you want to install Microsoft Graph module? [Y] Yes [N] No  
@@ -69,7 +69,7 @@ Function InstallMainAll {
 }
 
 Function InstallMainBasic {
-    $MsGraphBetaModule =  Get-Module Microsoft.Graph -ListAvailable
+    $MsGraphBetaModule =  Get-Module Microsoft.Graph.Authentication -ListAvailable
     if($null -eq $MsGraphBetaModule){ 
         Write-host "Important: Microsoft Graph module is unavailable. `nIt is mandatory to have this module installed in the system to run the script successfully." -ForegroundColor Yellow
         $confirm = Read-Host Are you sure you want to install Microsoft Graph module? [Y] Yes [N] No  
