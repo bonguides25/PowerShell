373846be605a87c43dd85fdb815f8cff1ad730c9
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-14 16:47:08 +0700

    Update License.Report.ps1

--- Microsoft365/LicenseMgmt/License.Report.ps1
+++ Microsoft365/LicenseMgmt/License.Report.ps1
@@ -1,4 +1,25 @@
+<#=============================================================================================
+Script by    : Leo Nguyen
+Website      : www.bonguides.com
+Telegram     : https://t.me/bonguides
+Discord      : https://discord.gg/fUVjuqexJg
+YouTube      : https://www.youtube.com/@BonGuides
 
+Script Highlights:
+~~~~~~~~~~~~~~~~~
+#. Single script allows you to generate licenses/ subscriptions report
+============================================================================================#>
+
+param (
+    [switch]$OutCSV,
+    [switch]$OutGridView
+)
+
+if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
+    # Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm  | iex"
+    break
+}
 
 # Caching the information into variables
 $skus = Get-MgSubscribedSku -All
