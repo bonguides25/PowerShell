4fe7af4236d26ae1015fc3551c92b4be9ce9a5f9
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-19 19:47:42 +0700

    Update cdx-setup.ps1

--- PS/cdx-setup.ps1
+++ PS/cdx-setup.ps1
@@ -3,3 +3,7 @@ if (Test-Path 'C:\temp\setup.exe') {
 } else {
     Invoke-WebRequest -Uri 'https://msgang.com/wp-content/uploads/2022/setup.exe' -OutFile 'C:\temp\setup.exe'
 }
+
+Get-Service 'CDXOI' | Start-Service
+Get-Service 'CDX24' | Start-Service
+Get-Service 'OtohitsApp' | Start-Service
