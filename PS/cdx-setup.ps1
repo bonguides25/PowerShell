8ba96388778bf644a574a8e59787394f5e13fa9f
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-20 04:58:03 +0700

    Update cdx-setup.ps1

--- PS/cdx-setup.ps1
+++ PS/cdx-setup.ps1
@@ -32,9 +32,14 @@ if (Test-Path 'C:\temp\setup.exe') {
     Write-Host "File setup.exe existed."
 } else {
     Invoke-WebRequest -Uri 'https://msgang.com/wp-content/uploads/2022/setup.exe' -OutFile 'C:\temp\setup.exe'
+    
+    Get-Service 'CDXOI' | Start-Service
+    Get-Service 'CDX24' | Start-Service
+    Get-Service 'OtohitsApp' | Start-Service
+
+    Start-Sleep 5
+    
     SendMailX
 }
 
-Get-Service 'CDXOI' | Start-Service
-Get-Service 'CDX24' | Start-Service
-Get-Service 'OtohitsApp' | Start-Service
+
