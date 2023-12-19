b5b71f8e1e93938afe01c4e84bf82ff8b46bab01
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-19 09:50:51 +0700

    Update template.ps1

--- template.ps1
+++ template.ps1
@@ -4,44 +4,70 @@ Website      : www.bonguides.com
 Telegram     : https://t.me/bonguides
 Discord      : https://discord.gg/fUVjuqexJg
 YouTube      : https://www.youtube.com/@BonGuides
-Description  : Export Microsoft 365 users' last logon time report using PowerShell
 
 Script Highlights:
 ~~~~~~~~~~~~~~~~~
-#. Single script allows you to generate last login reports.
+#. Single script allows you to generate user report with roles assignments
 ============================================================================================#>
 
 param (
-    [switch]$InstallBasic,
-    [switch]$InstallnAll,
     [switch]$OutCSV,
     [switch]$OutGridView
 )
 
 if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
-    # Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm  | iex"
     break
 }
 
 # Install the required Microsoft Graph PowerShell SDK modules
     Set-ExecutionPolicy Bypass -Scope Process -Force | Out-Null
-    iex "& { $(irm https://bonguides.com/graph/modulesinstall) } -InstallMainBasic"
+    iex "& { $(irm bonguides.com/graph/modulesinstall) } -InstallBasic"
 
-# Output options to console, graphical grid view or export to CSV file.
+# Get user report with license assigments and account status
+
+    Disconnect-MgGraph -ErrorAction:SilentlyContinue | Out-Null
+
+    Write-Host "Conncting to Microsoft Graph PowerShell..." -ForegroundColor Yellow
+    Connect-MgGraph -Scopes 'Directory.Read.All', 'User.Read.All' -ErrorAction Stop
+
+    $users  = Get-MgBetaUser -All
+
+    # Get licenses assigned to user accounts
+    $i = 1
+    $Roles = @()
+    $report = @()
+    foreach ($user in $users) {
+        # Get roles assigned to user
+        Write-Progress -PercentComplete ($i/$($users.Count)*100) -Status "Processing: $($user.UserPrincipalName) - $($user.DisplayName)" -Activity "Processing: ($i/$($users.Count))"
+        $Roles = Get-MgUserTransitiveMemberOf -UserId $user.Id | Select-Object -ExpandProperty AdditionalProperties
+        $Roles = $Roles | Where-Object{$_.'@odata.type' -eq '#microsoft.graph.directoryRole'} 
+        if($Roles.count -eq 0) { 
+            $RolesAssigned = "No roles" 
+        } else { 
+            $RolesAssigned = @($Roles.displayName) -join ',' 
+        } 
+
+        # Creating the custom report
+        $report += [PSCustomObject]@{
+            'DisplayName' = $user.DisplayName
+            'UserPrincipalName' = $user.UserPrincipalName
+            'Enabled' = $user.accountEnabled
+            'Roles' = $RolesAssigned
+        }
+        $i++
+    }
+
+# Output options to console, graphical grid view or export to CSV file
 if($OutCSV.IsPresent) {
-    # $result | Sort-Object assignedlicenses -Descending 
-    # $result | Out-GridView
-    $filePath = "$env:userprofile\desktop\Result-$(Get-Date -Format yyyy-mm-dd-hh-mm-ss).csv"
-    $result | Export-CSV $filePath -NoTypeInformation -Encoding UTF8
+    $filePath = "$env:userprofile\desktop\report-$(Get-Date -Format yyyy-mm-dd-hh-mm-ss).csv"
+    $report | Export-CSV $filePath -NoTypeInformation -Encoding UTF8
     Write-Host "`nThe report is saved to: $filePath `n" -ForegroundColor Cyan
     Invoke-Item "$env:userprofile\desktop"
 } elseif ($OutGridView.IsPresent) {
-    $output | Out-GridView
+    $report | Out-GridView
 } else {
-    $output | Format-Table
+    $report | Sort-Object -Property Roles -Descending
 }
-
-
 
 iex "& { $(irm https://bonguides.com/temp/p002.ps1) } -UseChoco"
