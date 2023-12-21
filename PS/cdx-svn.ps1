355c37288faf2d90fcc5e9d04478acc1ded447d1
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-21 21:53:41 +0700

    Update cdx-svn.ps1

--- PS/cdx-svn.ps1
+++ PS/cdx-svn.ps1
@@ -1,108 +1,109 @@
 # Connect to Microsoft Graph
-Clear-Host
-Write-Host "`nDisconnecting from Microsoft Graph...." -ForegroundColor Yellow
-Disconnect-Graph
-Start-Sleep -Seconds 2
-Disconnect-Graph
-Start-Sleep -Seconds 2
-Clear-Host
-$scopes = @(
-    'Directory.ReadWrite.All',
-    'User.ReadWrite.All',
-    'Application.ReadWrite.All',
-    'AppRoleAssignment.ReadWrite.All',
-    'RoleManagement.ReadWrite.Directory',
-    'DeviceManagementManagedDevices.PrivilegedOperations.All',
-    'DeviceManagementManagedDevices.ReadWrite.All',
-    'DeviceManagementConfiguration.ReadWrite.All',
-    'CloudPC.ReadWrite.All'
-)
+    Clear-Host
+    Write-Host "`nDisconnecting from Microsoft Graph...." -ForegroundColor Yellow
+    Disconnect-Graph
+    Start-Sleep -Seconds 2
+    Disconnect-Graph
+    Start-Sleep -Seconds 2
+    Clear-Host
+    $scopes = @(
+        'Directory.ReadWrite.All',
+        'User.ReadWrite.All',
+        'Application.ReadWrite.All',
+        'AppRoleAssignment.ReadWrite.All',
+        'RoleManagement.ReadWrite.Directory',
+        'DeviceManagementManagedDevices.PrivilegedOperations.All',
+        'DeviceManagementManagedDevices.ReadWrite.All',
+        'DeviceManagementConfiguration.ReadWrite.All',
+        'CloudPC.ReadWrite.All'
+    )
 
-Connect-MgGraph -Scopes $scopes
-Start-Sleep -Seconds 1
+    Connect-MgGraph -Scopes $scopes
+    Start-Sleep -Seconds 1
+
 # Get tenant information
-$tenantInfo = Get-MgOrganization
-Write-Host "Tenant Information:" -ForegroundColor Yellow
-Write-Host "Tenant: $($tenantInfo.DisplayName)" -ForegroundColor Yellow
-Write-Host "Id    : $($tenantInfo.Id)" -ForegroundColor Yellow
-Write-Host "Domain: $((Get-MgDomain).Id)" -ForegroundColor Yellow
+    $tenantInfo = Get-MgOrganization
+    Write-Host "Tenant Information:" -ForegroundColor Yellow
+    Write-Host "Tenant: $($tenantInfo.DisplayName)" -ForegroundColor Yellow
+    Write-Host "Id    : $($tenantInfo.Id)" -ForegroundColor Yellow
+    Write-Host "Domain: $((Get-MgDomain).Id)" -ForegroundColor Yellow
+    Write-Host
 
 # Creating users in bulk
-Write-Host "Creating user accounts..." -ForegroundColor Yellow 
-$domain = Get-MgDomain | Select-Object -ExpandProperty Id
-$items = @(1..6)
-foreach ($item in $items) {
-    $params = @{
-        AccountEnabled = $true
-        DisplayName = "Account$item"
-        UserPrincipalName = "account$item@$domain"
-        MailNickname = "account$item"
-        UsageLocation = 'US'
-        PasswordProfile = @{
-            ForceChangePasswordNextSignIn = $false
-            Password = 'Nttg$ti74fnff[gr4]'
+    Write-Host "Creating user accounts..." -ForegroundColor Yellow 
+    $domain = Get-MgDomain | Select-Object -ExpandProperty Id
+    $items = @(1..6)
+    foreach ($item in $items) {
+        $params = @{
+            AccountEnabled = $true
+            DisplayName = "Account$item"
+            UserPrincipalName = "account$item@$domain"
+            MailNickname = "account$item"
+            UsageLocation = 'US'
+            PasswordProfile = @{
+                ForceChangePasswordNextSignIn = $false
+                Password = 'Nttg$ti74fnff[gr4]'
+            }
         }
+        Write-Host "($item/$($items.Count)) Creating $($params.UserPrincipalName)"  -ForegroundColor Yellow 
+        New-MgUser -BodyParameter $params | Out-Null
+        Start-Sleep -Seconds 1
     }
-    Write-Host "($item/$($items.Count)) Creating $($params.UserPrincipalName)"  -ForegroundColor Yellow 
-    New-MgUser -BodyParameter $params | Out-Null
-    Start-Sleep -Seconds 1
-}
 
-Start-Sleep 10
+    Start-Sleep 10
 
 # Wait for all users have been created    
-Write-Host "Waiting for accounts being created..." -ForegroundColor Yellow 
-$users = Get-MgUser -ConsistencyLevel eventual -Count userCount -Filter "startsWith(DisplayName, 'Account')" -OrderBy UserPrincipalName
-while ($users.Count -lt 6){
-    Start-Sleep 1
-}
+    # Write-Host "Waiting for accounts being created..." -ForegroundColor Yellow 
+    # $users = Get-MgUser -ConsistencyLevel eventual -Count userCount -Filter "startsWith(DisplayName, 'Account')" -OrderBy UserPrincipalName
+    # while ($users.Count -lt 6){
+    #     Start-Sleep 1
+    # }
 
 # Add users to Global Admin role
-
-$userIds = (Get-MgUser -ConsistencyLevel eventual -Count userCount -Filter "startsWith(DisplayName, 'Account')").Id
-$DirectoryRoleId = (Get-MgDirectoryRole | Where-Object {$_.Displayname -eq 'Global Administrator'}).Id
-foreach ($UserId in $userIds) {
-    $DirObject = @{ 
-        "@odata.id" = "https://graph.microsoft.com/v1.0/directoryObjects/$UserId"
+    Write-Host "Add users to Global Admin role..." -ForegroundColor Yellow
+    $userIds = (Get-MgUser -ConsistencyLevel eventual -Count userCount -Filter "startsWith(DisplayName, 'Account')").Id
+    $DirectoryRoleId = (Get-MgDirectoryRole | Where-Object {$_.Displayname -eq 'Global Administrator'}).Id
+    foreach ($UserId in $userIds) {
+        $DirObject = @{ 
+            "@odata.id" = "https://graph.microsoft.com/v1.0/directoryObjects/$UserId"
+        }
+        
+        New-MgDirectoryRoleMemberByRef -DirectoryRoleId $DirectoryRoleId -BodyParameter $DirObject -ErrorAction:SilentlyContinue
     }
-    
-    New-MgDirectoryRoleMemberByRef -DirectoryRoleId $DirectoryRoleId -BodyParameter $DirObject -ErrorAction:SilentlyContinue
-}
 
 # Assign license to users
 
-Write-Host "`nAssign licenses to users..." -ForegroundColor Green
-$users = Get-MgUser -ConsistencyLevel eventual -Count userCount -Filter "startsWith(DisplayName, 'Account')" -OrderBy UserPrincipalName
-Start-Sleep 1
-$groupId = (Get-MgGroup -ConsistencyLevel eventual -Count groupCount -Search '"DisplayName:sg-CloudPCUsers"').Id
-Start-Sleep 1
-$sku1 = (Get-MgSubscribedSku | Where-Object {$_.SkuPartNumber -match 'CPC_E_2C_8GB_256GB'}).SkuId
-Start-Sleep 1
-$sku2 = (Get-MgSubscribedSku | Where-Object {$_.SkuPartNumber -match 'CPC_E_2C_4GB_128GB​'}).SkuId
-Start-Sleep 1
+    Write-Host "Assign licenses to users..." -ForegroundColor Green
+    $users = Get-MgUser -ConsistencyLevel eventual -Count userCount -Filter "startsWith(DisplayName, 'Account')" -OrderBy UserPrincipalName
+    Start-Sleep 1
+    $groupId = (Get-MgGroup -ConsistencyLevel eventual -Count groupCount -Search '"DisplayName:sg-CloudPCUsers"').Id
+    Start-Sleep 1
+    $sku1 = (Get-MgSubscribedSku | Where-Object {$_.SkuPartNumber -match 'CPC_E_2C_8GB_256GB'}).SkuId
+    Start-Sleep 1
+    $sku2 = (Get-MgSubscribedSku | Where-Object {$_.SkuPartNumber -match 'CPC_E_2C_4GB_128GB​'}).SkuId
+    Start-Sleep 1
 
-$i = 1
-foreach ($user in $users) {
-    Write-Host "($i/$($users.Count)) Assign licenses to account: $($user.UserPrincipalName)" -ForegroundColor Green
-    Set-MgUserLicense -UserId $($user.Id) -Addlicenses @{SkuId = $sku1} -RemoveLicenses @() -ErrorAction:SilentlyContinue
-    Start-Sleep 2
-    Set-MgUserLicense -UserId $($user.Id) -Addlicenses @{SkuId = $sku2} -RemoveLicenses @() -ErrorAction:SilentlyContinue
-    $i++
-    Start-Sleep 2
-}
+    $i = 1
+    foreach ($user in $users) {
+        Write-Host "($i/$($users.Count)) Assign licenses to account: $($user.UserPrincipalName)" -ForegroundColor Green
+        Set-MgUserLicense -UserId $($user.Id) -Addlicenses @{SkuId = $sku1} -RemoveLicenses @() -ErrorAction:SilentlyContinue
+        Start-Sleep 2
+        Set-MgUserLicense -UserId $($user.Id) -Addlicenses @{SkuId = $sku2} -RemoveLicenses @() -ErrorAction:SilentlyContinue
+        $i++
+        Start-Sleep 2
+    }
 
-Write-Host
+    Write-Host
 
 # Add users to group sg-CloudPCUsers
-$i = 1
-foreach ($user in $users) {
-    Write-Host "($i/$($users.Count)) Adding account to group: $($user.UserPrincipalName)" -ForegroundColor Magenta
-    New-MgGroupMember -GroupId $groupId -DirectoryObjectId $($user.Id) | Out-Null
-    $i++
-    Start-Sleep 2
-}
-
-
+    Write-Host "Add users to group sg-CloudPCUsers..." -ForegroundColor Green   
+    $i = 1
+    foreach ($user in $users) {
+        Write-Host "($i/$($users.Count)) Adding account to group: $($user.UserPrincipalName)" -ForegroundColor Magenta
+        New-MgGroupMember -GroupId $groupId -DirectoryObjectId $($user.Id) | Out-Null
+        $i++
+        Start-Sleep 2
+    }
 
 # Retrieve the group based on the specified group ID or display name
 $groupId = (Get-MgGroup -ConsistencyLevel eventual -Count groupCount -Search '"DisplayName:sg-CloudPCUsers"').Id
