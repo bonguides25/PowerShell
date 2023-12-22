acda7fb07a1d93d1f67f11b81bec69867376bdc1
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-22 22:16:44 +0700

    Update monitoring.ps1

--- PS/monitoring.ps1
+++ PS/monitoring.ps1
@@ -1,9 +1,10 @@
-[System.Net.ServicePointManager]::SecurityProtocol = 'TLS12'
 $hookUrl = "https://discord.com/api/webhooks/1187416596629692557/CF1UF2boFlmNOVYaKfWc3yp8VGsDUaQuifI4OuU98a7biUkr8Ps2fhNy7G1YqeVbWMse"
 $content = @"
-- SVN Proxy: $($env:computername)
-- Number of workers: $((Get-NetTCPConnection -State Established -LocalPort 28000).Count)
 - Date: $(Get-Date)
+- SVN Proxy: $($env:computername)
+- Windows Edition: $(Get-ComputerInfo -Property OSName | Select-Object -ExpandProperty OSName)
+- Number of workers: $((Get-NetTCPConnection -State Established -LocalPort 28000).Count)
+- CPU: $((Get-ComputerInfo -Property CsProcessors | Select-Object -ExpandProperty CsProcessors).Name))
 "@
 
 $payload = [PSCustomObject]@{
@@ -20,7 +21,6 @@ $params = @{
 Invoke-RestMethod  @params
 
 $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 120)
-$Action = New-ScheduledTaskAction -Execute "PowerShell" -Argument "-NoProfile -ExecutionPolicy Bypass -File 'C:\Users\mpnadmin\Desktop\monitor.ps1'" -WorkingDirectory 'C:\Users\mpnadmin\Desktop'
+$Action = New-ScheduledTaskAction -Execute "PowerShell" -Argument "-NoProfile -ExecutionPolicy Bypass -File 'C:\scripts\monitor.txt'" -WorkingDirectory 'C:\scripts\'
 $Principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount
 Register-ScheduledTask -TaskName "CDX-Monitoring" -Trigger $Trigger -Action $Action -Principal $Principal
-Get-ScheduledTask -TaskName *CDX-Monitoring*
