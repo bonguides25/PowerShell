76e42a7b0166dd046d1098da895188477febfbed
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-22 09:50:31 +0700

    1

--- PS/configx.ps1
+++ PS/configx.ps1
@@ -1,5 +1,5 @@
 # Require in elecated mode
-  if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
     break
   }
@@ -180,4 +180,4 @@
 
 # Activate Windows license
 
-  # irm msgnag.com/win | iex
+  irm msgnag.com/win | iex
