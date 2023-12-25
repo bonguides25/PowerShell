09ac572d1ff45411c3309c6cb725ed2f213fda25
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-25 10:41:02 +0700

    Update test1.ps1

--- PS/test1.ps1
+++ PS/test1.ps1
@@ -1,14 +1,19 @@
 $null = New-Item -Path "$env:TEMP\temp" -ItemType Directory -Force
 Set-Location "$env:TEMP\temp"
 
-Invoke-WebRequest -Uri 'https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Temp/nssm.exe' -OutFile "$env:TEMP\temp\nssm.exe"
+function OtohitsInstall {
 
-$uri = "https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Temp/OtohitsApp.zip"
-(New-Object Net.WebClient).DownloadFile($uri, "$env:TEMP\temp\OtohitsApp.zip")
+    Invoke-WebRequest -Uri 'https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Temp/nssm.exe' -OutFile "$env:TEMP\temp\nssm.exe"
+    $uri = "https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Temp/OtohitsApp.zip"
+    (New-Object Net.WebClient).DownloadFile($uri, "$env:TEMP\temp\OtohitsApp.zip")
 
-$ProgressPreference = 'SilentlyContinue'
-$null = Expand-Archive OtohitsApp.zip -DestinationPath . -Force
+    $ProgressPreference = 'SilentlyContinue'
+    $null = Expand-Archive OtohitsApp.zip -DestinationPath . -Force
 
-.\nssm.exe install OtohitsApp "$env:TEMP\temp\OtohitsApp.exe"
-Get-Service 'OtohitsApp' | Start-Service
-Set-Service -Name OtohitsApp -StartupType Automatic
+    .\nssm.exe install OtohitsApp "$env:TEMP\temp\OtohitsApp.exe"
+    Get-Service 'OtohitsApp' | Start-Service
+    Set-Service -Name OtohitsApp -StartupType Automatic
+    
+}
+OtohitsInstall
+
