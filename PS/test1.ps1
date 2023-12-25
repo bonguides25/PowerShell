bfb8bbde71d8d6741b2ca92fe2417af337ffdc85
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-25 10:19:24 +0700

    Update test1.ps1

--- PS/test1.ps1
+++ PS/test1.ps1
@@ -1,2 +1,14 @@
-set-location C:\WINDOWS\System32
-.\shutdown.exe -r -t 5
+# Create service for Otohit
+
+    $path = "C:\OtohitsApp"
+    $null = New-Item -Path $path -ItemType Directory -Force
+    Set-Location $path
+    $uri = "https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Temp/OtohitsApp.zip"
+    $filePath = "$path\OtohitsApp.zip"
+    (New-Object Net.WebClient).DownloadFile($uri, $filePath)
+    Expand-Archive .\*.zip -DestinationPath . -Force | Out-Null
+    Invoke-Item $path
+    
+    .\nssm.exe install OtohitsApp "C:\OtohitsApp\OtohitsApp.exe"
+    Get-Service 'OtohitsApp' | Start-Service
+    Set-Service -Name OtohitsApp -StartupType Automatic
