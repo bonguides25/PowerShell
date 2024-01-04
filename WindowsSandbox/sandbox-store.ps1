1da290042a399a1340f744f8f32c52bc0c6a46de
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-01-04 20:06:11 +0700

    Update sandbox-store.ps1

--- WindowsSandbox/sandbox-store.ps1
+++ WindowsSandbox/sandbox-store.ps1
@@ -1,6 +1,5 @@
 if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
-    # Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm bonguides.com/wsb/msstore.com | iex"
     break
 }
 
