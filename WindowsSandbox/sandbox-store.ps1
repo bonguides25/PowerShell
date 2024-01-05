ac55459fc0894a027a34919d1f6d461e116a59d7
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-01-05 09:20:31 +0700

    Update sandbox-store.ps1

--- WindowsSandbox/sandbox-store.ps1
+++ WindowsSandbox/sandbox-store.ps1
@@ -1,78 +1,39 @@
+<#=============================================================================================
+Script by    : Leo Nguyen
+Website      : www.bonguides.com
+Telegram     : https://t.me/bonguides
+Discord      : https://discord.gg/fUVjuqexJg
+YouTube      : https://www.youtube.com/@BonGuides
+
+Script Highlights:
+~~~~~~~~~~~~~~~~~
+#. Install Microsoft Store on Windows Sandbox (winget).
+============================================================================================#>
+
 if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
     break
 }
 
-if ([System.Environment]::OSVersion.Version.Build -lt 16299) {
-    Write-Host "This pack is for Windows 10 version 1709 and later"
-    Write-Host "Exitting..."
-    Start-Sleep -Seconds 3
-    exit
-}
-
-# Create temporary directory
-$null = New-Item -Path $env:temp\temp -ItemType Directory -Force
-Set-Location $env:temp\temp
-
-# Download required files
-Write-Host
-Write-Host "Installing dependency packages..." -ForegroundColor Green
-$uri = "https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/bonben365.com/Zip/microsoftstore-win-ltsc.zip"
-(New-Object Net.WebClient).DownloadFile($uri, "$env:temp\temp\microsoftstore-win-ltsc.zip")
-
-# Extract downloaded file then run the script
-Expand-Archive .\microsoftstore-win-ltsc.zip -Force -ErrorAction:SilentlyContinue
-Set-Location "$env:temp\temp\microsoftstore-win-ltsc"
-
-if ([System.Environment]::Is64BitOperatingSystem -like "True") {
-    $arch = "x64"
-} else {
-    $arch = "x86"
-}
-
-if (!(Get-ChildItem "*WindowsStore*")) {    
-    Write-Host "Error: Required files are missing in the current directory"
-    Write-Host "Exitting..."
-    Start-Sleep -Seconds 3
-    exit
-}
-
-if ($arch -eq "x86") {
-    $depens = Get-ChildItem | Where-Object {($_.Name -match '^*Microsoft.NET.Native*|^*VCLibs*') -and ($_.Name -like '*x86*')}
-} 
-if ($arch -eq "x64") {
-    $depens = Get-ChildItem | Where-Object {$_.Name -match '^*Microsoft.NET.Native*|^*VCLibs*'}
-}
-
+Set-Location $env:temp
 $progressPreference = 'silentlyContinue'
-foreach ($depen in $depens) {
-    Add-AppxPackage -Path "$depen" -ErrorAction:SilentlyContinue
-}
+Write-Host "`nInstalling Microsoft Store and its dependencies..."
 
-Write-Host "Installing Microsoft Store..." -ForegroundColor Green
-$null = Add-AppxProvisionedPackage -Online -PackagePath "$(Get-ChildItem | Where-Object { ($_.Name -like '*WindowsStore*') -and ($_.Name -like '*AppxBundle*') })" -LicensePath "$(Get-ChildItem | Where-Object { ($_.Name -like '*WindowsStore*') -and ($_.Name -like '*xml*') })"
+Invoke-WebRequest -Uri 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx' -OutFile 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
+Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx' -OutFile 'Microsoft.UI.Xaml.2.7.x64.appx'
+Invoke-WebRequest -Uri 'https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Files/StoreApps/Microsoft.WindowsStore_11809.1001.713.0_neutral_~_8wekyb3d8bbwe.AppxBundle' -OutFile 'Microsoft.WindowsStore_11809.1001.713.0_neutral_~_8wekyb3d8bbwe.AppxBundle'
+Invoke-WebRequest -Uri 'https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Files/StoreApps/Microsoft.NET.Native.Runtime.1.6_1.6.24903.0_x64__8wekyb3d8bbwe.Appx' -OutFile 'Microsoft.NET.Native.Runtime.1.6_1.6.24903.0_x64__8wekyb3d8bbwe.Appx'
+Invoke-WebRequest -Uri 'https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Files/StoreApps/Microsoft.NET.Native.Framework.1.6_1.6.24903.0_x64__8wekyb3d8bbwe.Appx' -OutFile 'Microsoft.NET.Native.Framework.1.6_1.6.24903.0_x64__8wekyb3d8bbwe.Appx'
 
-if ((Get-ChildItem "*StorePurchaseApp*")) {    
-$null = Add-AppxProvisionedPackage -Online -PackagePath "$(Get-ChildItem | Where-Object { ($_.Name -like '*StorePurchaseApp*') -and ($_.Name -like '*AppxBundle*') })" -LicensePath "$(Get-ChildItem | Where-Object { ($_.Name -like '*StorePurchaseApp*') -and ($_.Name -like '*xml*') })"
-}
+Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
 
-if ((Get-ChildItem "*DesktopAppInstaller*")) {    
-$null = Add-AppxProvisionedPackage -Online -PackagePath "$(Get-ChildItem | Where-Object { ($_.Name -like '*DesktopAppInstaller*') -and ($_.Name -like '*AppxBundle*') })" -LicensePath "$(Get-ChildItem | Where-Object { ($_.Name -like '*DesktopAppInstaller*') -and ($_.Name -like '*xml*') })"
-}
+Add-AppxPackage 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
+Add-AppxPackage 'Microsoft.UI.Xaml.2.7.x64.appx'
+Add-AppxPackage 'Microsoft.NET.Native.Framework.1.6_1.6.24903.0_x64__8wekyb3d8bbwe.Appx'
+Add-AppxPackage 'Microsoft.NET.Native.Runtime.1.6_1.6.24903.0_x64__8wekyb3d8bbwe.Appx'
+Add-AppxPackage 'Microsoft.WindowsStore_11809.1001.713.0_neutral_~_8wekyb3d8bbwe.AppxBundle'
 
-if ((Get-ChildItem "*XboxIdentityProvider*")) {
-$null = Add-AppxProvisionedPackage -Online -PackagePath "$(Get-ChildItem | Where-Object { ($_.Name -like '*XboxIdentityProvider*') -and ($_.Name -like '*AppxBundle*') })" -LicensePath "$(Get-ChildItem | Where-Object { ($_.Name -like '*XboxIdentityProvider*') -and ($_.Name -like '*xml*') })"
-}
-
-# Installed apps
-$packages = @("WindowsStore")
-$report = ForEach ($package in $packages){Get-AppxPackage -Name *$package* | select Name,Version,Status }
-write-host "`nInstalled packages:"
-$report | format-table
+Add-AppxPackage 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
 
 # Cleanup
-Set-Location "$env:temp"
-Remove-Item $env:temp\temp -Recurse -Force
 
-Write-Host "Done." -ForegroundColor Green
-Write-Host 
