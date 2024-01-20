0ab06c6621a2f180cfcafca148cf59f38ba92096
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-01-20 21:41:23 +0700

    Update TenantAvailabilityCheck.ps1

--- Microsoft365/Common/TenantAvailabilityCheck.ps1
+++ Microsoft365/Common/TenantAvailabilityCheck.ps1
@@ -4,61 +4,33 @@ Website      : www.bonguides.com
 Telegram     : https://t.me/bonguides
 Discord      : https://discord.gg/fUVjuqexJg
 YouTube      : https://www.youtube.com/@BonGuides
-Description  : Install Exchange Online PowerShell modules
+Description  : Verify Azure AD Tenant Availability
 ============================================================================================#>
 
-# Required running with elevated right.
-    if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-        Write-Warning "`nYou need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
-        # Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm  | iex"
-        break
+$domainName = $(Write-Host -NoNewLine) + $(Write-Host "`nProvide the domain name: " -ForegroundColor Yellow -NoNewLine; Read-Host)
+$FQDN = $domainName + ".onmicrosoft.com"
+
+if ($FQDN -notmatch "(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)") {
+    Write-Error -Message ("FQDN used incorrect format: '{0}'." -f $FQDN) -ErrorAction Stop
+}
+
+$uri = "https://login.microsoftonline.com/{0}/FederationMetadata/2007-06/FederationMetadata.xml" -f $FQDN
+
+$response = try { 
+    (Invoke-WebRequest -Uri $uri -Method GET -ErrorAction Stop).BaseResponse
+} catch [System.Net.WebException] { 
+    $_.Exception.Response 
+}
+
+switch ($response.StatusCode.Value__) {
+    200 {
+        Write-Host "The tenant name `"$FQDN`" is unavailable." -ForegroundColor Red
     }
-
-# Configure the system before install the module
-    Function InstallDeps {
-
-        # Configure Execution Policy
-            Set-ExecutionPolicy Bypass -Scope Process -Force | Out-Null
-
-        # Update the NuGet Provider if needed.
-            $nuGetPath = "C:\Program Files\PackageManagement\ProviderAssemblies\nuget\*\Microsoft.PackageManagement.NuGetProvider.dll"
-            $testPath = Test-Path -Path $nuGetPath
-            if ($testPath -match 'false') {
-                Write-Host "Installing NuGet Provider..." -ForegroundColor Yellow
-                Install-PackageProvider -Name NuGet -Force | Out-Null
-            }
-
-        # Update the PowerShellGet if needed.
-            $PSGetCurrentVersion = (Get-PackageProvider -Name 'PowerShellGet').Version
-            $PSGetLatestVersion = (Find-Module PowerShellGet).Version
-            if ($PSGetCurrentVersion -lt $PSGetLatestVersion) {
-                Write-Host "Updating PowerShellGet Module from $PSGetCurrentVersion to $PSGetLatestVersion..." -ForegroundColor Yellow
-                Install-Module -Name 'PowerShellGet' -Force
-            }
-
-        # We're installing from the PowerShell Gallery so make sure that it's trusted.
-            $InstallationPolicy = (Get-PSRepository -Name PSGallery).InstallationPolicy
-            if ($InstallationPolicy -match "Untrusted") {
-                Write-host "Configuring the PowerShell Gallery Repository..." -ForegroundColor Yellow
-                Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-            }
+    404 {
+        Write-Host "The tenant name `"$FQDN`" is available." -ForegroundColor Green
     }
-
-# Install the PowerShell module
-    $exoModule =  Get-Module ExchangeOnlineManagement -ListAvailable
-    if($null -eq $exoModule)
-    { 
-        Write-host "Important: Exchange Online module is unavailable. `nIt is mandatory to have this module installed in the system to run the script successfully." -ForegroundColor Yellow
-        $confirm = Read-Host Are you sure you want to install Exchange Online module? [Y] Yes [N] No  
-        if($confirm -match "[yY]") 
-        { 
-            Write-host "Installing Exchange Online module..." -ForegroundColor Yellow
-            Install-Module -Name ExchangeOnlineManagement -Scope CurrentUser
-            Write-host "Exchange Online Module is installed in the machine successfully" -ForegroundColor Magenta 
-        } 
-        else
-        { 
-            Write-host "Exiting. `nNote: Exchange Online module must be available in your system to run the script" 
-            Exit 
-        } 
+    default {
+        Write-Error -Message "Unable to determine result." -ErrorAction Stop
+        break;
+    }
 }
