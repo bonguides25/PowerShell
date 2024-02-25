a82480496d3ee4a08eeef91c495f83b8410973a7
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-02-25 10:27:52 +0700

    Update terminal.ps1

--- Utilities/terminal.ps1
+++ Utilities/terminal.ps1
@@ -1,1 +1,32 @@
+if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
+    # Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm bonguides.com/wsb/msstore.com | iex"
+    break
+}
 
+#Install and update Desktop framework packages
+$progressPreference = 'silentlyContinue'
+Set-Location $env:temp
+Write-Host "Instaling Microsoft Windows Terminal...`n"
+Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+Invoke-WebRequest -Uri 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx' -OutFile 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
+Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx' -OutFile 'Microsoft.UI.Xaml.2.7.x64.appx'
+Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.5/Microsoft.UI.Xaml.2.8.x64.appx' -OutFile 'Microsoft.UI.Xaml.2.8.x64.appx'
+
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+
+#Download and install Windows Terminal
+$url = 'https://github.com/microsoft/terminal/releases/latest'
+$request = [System.Net.WebRequest]::Create($url)
+$response = $request.GetResponse()
+$tagUrl = $response.ResponseUri.OriginalString
+$version = $tagUrl.split('/')[-1].Trim('v')
+$fileName = "Microsoft.WindowsTerminal_$($version)_8wekyb3d8bbwe.msixbundle"
+$downloadUrl = $tagUrl.Replace('tag', 'download') + '/' + $fileName
+(New-Object Net.WebClient).DownloadFile($downloadUrl, "$env:temp\WindowsTerminal.msixbundle")
+
+#Install Windows Terminal
+Add-AppxPackage WindowsTerminal.msixbundle
