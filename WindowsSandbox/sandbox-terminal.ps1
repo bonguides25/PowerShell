538dd0b86c2c890aeb69c219f38ba81b41fea22a
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-04-04 11:40:08 +0700

    Update sandbox-terminal.ps1

--- WindowsSandbox/sandbox-terminal.ps1
+++ WindowsSandbox/sandbox-terminal.ps1
@@ -1,33 +1,46 @@
+<#=============================================================================================
+Script by    : Leo Nguyen
+Website      : www.bonguides.com
+Telegram     : https://t.me/bonguides
+Discord      : https://discord.gg/fUVjuqexJg
+YouTube      : https://www.youtube.com/@BonGuides
+
+Script Highlights:
+~~~~~~~~~~~~~~~~~
+#. Install Microsoft Windows Terminal on Windows Sandbox.
+============================================================================================#>
+
 if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
-    # Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm bonguides.com/wsb/msstore.com | iex"
     break
 }
 
-#Install and update Desktop framework packages
-$progressPreference = 'silentlyContinue'
-Set-Location $env:temp
-Write-Host "`nInstalling dependencies..."
-Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
-Invoke-WebRequest -Uri 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx' -OutFile 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
-Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx' -OutFile 'Microsoft.UI.Xaml.2.7.x64.appx'
-Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.5/Microsoft.UI.Xaml.2.8.x64.appx' -OutFile 'Microsoft.UI.Xaml.2.8.x64.appx'
+Set-Location "$env:temp"
+Write-Host "`nInstalling Microsoft Windows Terminal..." -ForegroundColor Green
 
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
-Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
-Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+# Install C++ Runtime framework packages for Desktop Bridge
+    $ProgressPreference='Silent'
+    irm https://raw.githubusercontent.com/bonguides25/PowerShell/main/Utilities/msvclibs.ps1 | iex
 
-#Download and install Windows Terminal
-Write-Host "Instaling Microsoft Windows Terminal...`n"
-$url = 'https://github.com/microsoft/terminal/releases/latest'
-$request = [System.Net.WebRequest]::Create($url)
-$response = $request.GetResponse()
-$tagUrl = $response.ResponseUri.OriginalString
-$version = $tagUrl.split('/')[-1].Trim('v')
-$fileName = "Microsoft.WindowsTerminal_$($version)_8wekyb3d8bbwe.msixbundle"
-$downloadUrl = $tagUrl.Replace('tag', 'download') + '/' + $fileName
-(New-Object Net.WebClient).DownloadFile($downloadUrl, "$env:temp\WindowsTerminal.msixbundle")
+# Install Microsoft.UI.Xaml
+    $ProgressPreference='Silent'
+    irm https://raw.githubusercontent.com/bonguides25/PowerShell/main/Utilities/microsoft.ui.xaml.ps1 | iex
 
-#Install Windows Terminal
-Add-AppxPackage WindowsTerminal.msixbundle
+# Downlaod and install Windows Package Manager to install Store apps
+    Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+    Add-AppxPackage 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+
+# Download and install Windows Terminal
+
+    $url = 'https://github.com/microsoft/terminal/releases/latest'
+    $request = [System.Net.WebRequest]::Create($url)
+    $response = $request.GetResponse()
+    $tagUrl = $response.ResponseUri.OriginalString
+    $version = $tagUrl.split('/')[-1].Trim('v')
+    $fileName = "Microsoft.WindowsTerminal_$($version)_8wekyb3d8bbwe.msixbundle"
+    $downloadUrl = $tagUrl.Replace('tag', 'download') + '/' + $fileName
+    (New-Object Net.WebClient).DownloadFile($downloadUrl, "$env:temp\WindowsTerminal.msixbundle")
+
+    Add-AppxPackage WindowsTerminal.msixbundle
+
+ Get-AppxPackage | Where-Object { $_.Name -like "*terminal*"} | select Name, Architecture, Version, PublisherId -ErrorAction SilentlyContinue
