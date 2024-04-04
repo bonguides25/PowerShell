2988e7f04e4870875527fe2d1726634d0575ad4e
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-04-04 13:43:50 +0700

    Update msstore.ps1

--- Utilities/msstore.ps1
+++ Utilities/msstore.ps1
@@ -20,6 +20,7 @@ if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdenti
     if (Test-Path 'C:\Users\WDAGUtilityAccount') {
         Write-Host "`nYou're using Windows Sandbox." -ForegroundColor Yellow
         https://raw.githubusercontent.com/bonguides25/PowerShell/main/WindowsSandbox/sandbox-store.ps1
+        exit
     }
 
 # Installing the Microsoft Store on Windows LTSC
@@ -27,6 +28,7 @@ if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdenti
     if ($edition -like "*LTSC*"){
         Write-Host "`nYou're using $edition." -ForegroundColor Yellow
         irm https://raw.githubusercontent.com/bonguides25/PowerShell/main/WindowsLTSC/ltsc-store.ps1 | iex
+        exit
     }
 
 
