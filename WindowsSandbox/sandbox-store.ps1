e15c05ebda5d72b6402f817b777f33a272298636
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-04-04 11:38:09 +0700

    Update sandbox-store.ps1

--- WindowsSandbox/sandbox-store.ps1
+++ WindowsSandbox/sandbox-store.ps1
@@ -38,6 +38,6 @@ Write-Host "`nInstalling Microsoft Store..." -ForegroundColor Green
     Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
     Add-AppxPackage 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
 
-Get-AppxPackage | Where-Object { $_.name -like "*Store*" -or $_.name -like "*UI.Xaml*" -or $_.name -like "*DesktopAppInstaller*" } | Select-Object Name, Version -ErrorAction SilentlyContinue
+Get-AppxPackage | Where-Object { $_.Name -like "*terminal*"} | select Name, Architecture, Version, PublisherId -ErrorAction SilentlyContinue
 Write-Host "Done.`n" -ForegroundColor Green
 
