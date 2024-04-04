724d07494b81c681e259e3b97a11bd9432af25d2
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-04-04 10:08:00 +0700

    Update msvclibs.ps1

--- Utilities/msvclibs.ps1
+++ Utilities/msvclibs.ps1
@@ -6,8 +6,8 @@ if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdenti
 
 # Install C++ Runtime framework packages for Desktop Bridge
 $ProgressPreference='Silent'
-(New-Object Net.WebClient).DownloadFile('https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx', "$env:temp\temp\Microsoft.VCLibs.x64.14.00.Desktop.appx")
-Add-AppxPackage -Path Microsoft.VCLibs.x64.14.00.Desktop.appx -ErrorAction SilentlyContinue | Out-Null
+(New-Object Net.WebClient).DownloadFile('https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx', "$env:temp\Microsoft.VCLibs.x64.14.00.Desktop.appx")
+Add-AppxPackage -Path "$env:temp\Microsoft.VCLibs.x64.14.00.Desktop.appx" -ErrorAction SilentlyContinue | Out-Null
 
 (New-Object Net.WebClient).DownloadFile('https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Files/StoreApps/Microsoft.VCLibs.140.00_14.0.32530.0_x64__8wekyb3d8bbwe.Appx', "$env:temp\Microsoft.VCLibs_x64.Appx")
 Add-AppxPackage -Path "$env:temp\Microsoft.VCLibs_x64.Appx" -ErrorAction SilentlyContinue | Out-Null
