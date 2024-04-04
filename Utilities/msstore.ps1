be7ea08ca5d99b07a9cf957a5921b81fe160e788
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-04-04 13:35:14 +0700

    Update msstore.ps1

--- Utilities/msstore.ps1
+++ Utilities/msstore.ps1
@@ -7,13 +7,21 @@ YouTube      : https://www.youtube.com/@BonGuides
 
 Script Highlights:
 ~~~~~~~~~~~~~~~~~
-#. Install Microsoft Store on Windows Sandbox (winget).
+# Install Microsoft Store on Windows 10, 11
+# Including Windows Sandbox and Windows LTSC systems
 ============================================================================================#>
 
 if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
     break
 }
+
+if (Test-Path 'C:\Users\WDAGUtilityAccount') {
+    Write-Host "`nYou're using Windows Sandbox." -ForegroundColor Yellow
+    https://raw.githubusercontent.com/bonguides25/PowerShell/main/WindowsSandbox/sandbox-store.ps1
+}
+
+
 
 # Create temporary directory
 $null = New-Item -Path $env:temp\temp -ItemType Directory -Force
