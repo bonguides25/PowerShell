30947992be74a5e3bb4d715f2e8aedc09e487dc9
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-04-12 05:13:18 +0700

    Create omp.ps1

---
+++ Utilities/omp.ps1
@@ -0,0 +1,9 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+Start-Sleep -Seconds 1
+
+#Update the $env:Path to the current session
+$userpath = [System.Environment]::GetEnvironmentVariable("Path","User")
+$machinePath = [System.Environment]::GetEnvironmentVariable("Path","Machine")
+$env:Path = $userpath + ";" + $machinePath
+
+oh-my-posh font install AnonymousPro
