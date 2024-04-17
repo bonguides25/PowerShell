1a734480a65dd1ed3ec2dc8d47d47493a6467f28
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-04-17 08:20:45 +0700

    Update configx.ps1

--- PS/configx.ps1
+++ PS/configx.ps1
@@ -222,6 +222,27 @@ irm bonguides.com/winget | iex
 # Set-ItemProperty $reg_path -Name NoAutoUpdate -Value 1
 # Set-ItemProperty $reg_path -Name AUOptions -Value 3
 
+$uri = "https://github.com/bonben365/office-installer/raw/main/setup.exe"
+$productId = "O365ProPlusRetail"
+$languageId = "en-US"
+$arch = '64'
+
+$workingDir = New-Item -Path $env:temp\ClickToRun\$productId -ItemType Directory -Force
+Set-Location $workingDir
+
+$configurationFile = "configuration-x$arch.xml"
+New-Item $configurationFile -ItemType File -Force | Out-Null
+Add-Content $configurationFile -Value "<Configuration>"
+Add-content $configurationFile -Value "<Add OfficeClientEdition=`"$arch`">"
+Add-content $configurationFile -Value "<Product ID=`"$productId`">"
+Add-content $configurationFile -Value "<Language ID=`"$languageId`"/>"
+Add-Content $configurationFile -Value "</Product>"
+Add-Content $configurationFile -Value "</Add>"
+Add-Content $configurationFile -Value "</Configuration>"
+
+(New-Object Net.WebClient).DownloadFile($uri, "$workingDir\ClickToRun.exe")
+
+Start-Process -FilePath .\ClickToRun.exe -ArgumentList "/configure .\$($configurationFile)" -NoNewWindow -Wait
 
 Write-Host "Completed..." -ForegroundColor Yellow
 Write-Host "Restarting..." -ForegroundColor Yellow
