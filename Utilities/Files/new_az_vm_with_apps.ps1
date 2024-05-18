5a50e1c71ea8beb11ac4d4aaf5ee5f08e7430b67
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-05-18 07:48:56 +0700

    Update new_az_vm_with_apps.ps1

--- Utilities/Files/new_az_vm_with_apps.ps1
+++ Utilities/Files/new_az_vm_with_apps.ps1
@@ -5,16 +5,16 @@ irm https://community.chocolatey.org/install.ps1 | iex
 RefreshEnv
 Set-Location 'C:\ProgramData\chocolatey\bin'
 .\choco.exe feature enable -n allowGlobalConfirmation
-.\choco.exe install oh-my-posh -y
+# .\choco.exe install oh-my-posh -y
 .\choco.exe install GoogleChrome -y --ignore-check
 .\choco.exe install firefox -y
 .\choco.exe install vlc -y
-.\choco.exe install winscp -y
-.\choco.exe install mremoteng -y
+# .\choco.exe install winscp -y
+# .\choco.exe install mremoteng -y
 .\choco.exe install zoom -y
-.\choco.exe install discord -y
+# .\choco.exe install discord -y
 .\choco.exe install skype -y
 .\choco.exe install thunderbird -y
 .\choco.exe install fscapture -y
 .\choco.exe install teamviewer -y
-.\choco.exe install VisualStudioCode -y
+# .\choco.exe install VisualStudioCode -y
