f8e5e99196abd4f3002d33a788f6758b9516b39b
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-05-26 16:36:29 +0700

    Update wsb_apps.ps1

--- Utilities/Files/wsb_apps.ps1
+++ Utilities/Files/wsb_apps.ps1
@@ -1,1 +1,16 @@
+# Installing Chocolatey package manager
+Set-ExecutionPolicy Bypass -Scope Process -Force
+irm https://community.chocolatey.org/install.ps1 | iex
 
+RefreshEnv
+Set-Location 'C:\ProgramData\chocolatey\bin'
+.\choco.exe feature enable -n allowGlobalConfirmation
+.\choco.exe install GoogleChrome -y --ignore-check
+.\choco.exe install firefox -y
+.\choco.exe install vlc -y
+.\choco.exe install winscp -y
+.\choco.exe install zoom -y
+.\choco.exe install thunderbird -y
+.\choco.exe install fscapture -y
+.\choco.exe install teamviewer -y
+.\choco.exe install VisualStudioCode -y
