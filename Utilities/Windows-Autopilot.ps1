de57905de2c7b9a6e04b38277da482ca40bd82f8
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-05-30 21:11:03 +0700

    Update Windows-Autopilot.ps1

--- Utilities/Windows-Autopilot.ps1
+++ Utilities/Windows-Autopilot.ps1
@@ -1,7 +1,8 @@
 
 Write-Host "Installing the WindowsAutopilotInfo script..."
 PowerShell.exe -ExecutionPolicy Bypass
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+Install-PackageProvider -Name NuGet -Force
 Install-Script -name Get-WindowsAutopilotInfo -Force
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
 Write-Host "Adding the device to Windows Autopilot..."
 Get-WindowsAutopilotInfo -Online
