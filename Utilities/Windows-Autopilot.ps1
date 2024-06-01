8c4631a64384d702dcf8dda1d4407894f8ff88df
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-06-01 09:44:36 +0700

    Update Windows-Autopilot.ps1

--- Utilities/Windows-Autopilot.ps1
+++ Utilities/Windows-Autopilot.ps1
@@ -1,8 +1,8 @@
 
-Write-Host "Installing the WindowsAutopilotInfo script..."
+Write-Host "Installing the WindowsAutopilotInfo script..." -ForegroundColor Yellow
 Set-ExecutionPolicy -ExecutionPolicy Bypass
 Install-PackageProvider -Name NuGet -Force | Out-Null
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 Install-Script -name Get-WindowsAutopilotInfo -Force
-Write-Host "Adding the device to Windows Autopilot..."
 Get-WindowsAutopilotInfo -Online
+Write-Host "Adding the device to Windows Autopilot..." -ForegroundColor Yellow
