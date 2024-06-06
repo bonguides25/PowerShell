40a7e8ec551e6e75bf134135556fdbfbe9eebe76
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-06-06 20:14:51 +0700

    Create ps-dependencies.ps1

---
+++ Utilities/ps-dependencies.ps1
@@ -0,0 +1,30 @@
+# Required running with elevated right
+if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "`nYou need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
+    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "irm  | iex"
+    break
+}
+
+# Configure Execution Policy
+Set-ExecutionPolicy Bypass -Scope Process -Force | Out-Null
+
+# Update the NuGet Provider if needed
+$nuGetPath = "C:\Program Files\PackageManagement\ProviderAssemblies\nuget\*\Microsoft.PackageManagement.NuGetProvider.dll"
+$testPath = Test-Path -Path $nuGetPath
+if ($testPath -match 'false') {
+    Install-PackageProvider -Name NuGet -Force | Out-Null
+}
+
+# Update the PowerShellGet if needed
+$PSGetCurrentVersion = (Get-PackageProvider -Name 'PowerShellGet').Version
+$PSGetLatestVersion = (Find-Module PowerShellGet).Version
+if ($PSGetCurrentVersion -lt $PSGetLatestVersion) {
+    Install-Module -Name 'PowerShellGet' -Force
+}
+
+# We're installing from the PowerShell Gallery so make sure that it's trusted
+$InstallationPolicy = (Get-PSRepository -Name PSGallery).InstallationPolicy
+if ($InstallationPolicy -match "Untrusted") {
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+}
+
