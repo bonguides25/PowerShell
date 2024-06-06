c3c73a0ba1914a1968fbfe8097910cc1883401d7
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-06-06 21:59:41 +0700

    Update translator.ps1

--- Microsoft365/LicenseMgmt/translator.ps1
+++ Microsoft365/LicenseMgmt/translator.ps1
@@ -11,7 +11,8 @@ param (
 )
 
 $skus = Get-MgSubscribedSku -All
-$translationTable = Invoke-RestMethod -Method GET -Uri "https://download.microsoft.com/download/e/3/e/e3e9faf2-f28b-490a-9ada-c6089a1fc5b0/Product%20names%20and%20service%20plan%20identifiers%20for%20licensing.csv" | ConvertFrom-Csv
+Invoke-WebRequest -Uri "https://download.microsoft.com/download/e/3/e/e3e9faf2-f28b-490a-9ada-c6089a1fc5b0/Product%20names%20and%20service%20plan%20identifiers%20for%20licensing.csv" -OutFile "$env:temp\LicenseNames.csv"
+$translationTable = Import-Csv "$env:temp\LicenseNames.csv"
 
 $output = @()
 foreach ($sku in $skus) {
@@ -28,8 +29,6 @@ foreach ($sku in $skus) {
 }
 
 if($OutCSV.IsPresent) {
-    # $result | Sort-Object assignedlicenses -Descending 
-    # $result | Out-GridView
     $filePath = "$env:userprofile\desktop\Result-$(Get-Date -Format yyyy-mm-dd-hh-mm-ss).csv"
     $result | Export-CSV $filePath -NoTypeInformation -Encoding UTF8
     Write-Host "`nThe report is saved to: $filePath `n" -ForegroundColor Cyan
