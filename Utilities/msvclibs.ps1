5df7e50478ae015feccbbd6e28e8d7c5fd696782
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-06-11 09:16:23 +0700

    Update msvclibs.ps1

--- Utilities/msvclibs.ps1
+++ Utilities/msvclibs.ps1
@@ -9,5 +9,5 @@ $ProgressPreference='Silent'
 (New-Object Net.WebClient).DownloadFile('https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx', "$env:temp\Microsoft.VCLibs.x64.14.00.Desktop.appx")
 Add-AppxPackage -Path "$env:temp\Microsoft.VCLibs.x64.14.00.Desktop.appx" -ErrorAction SilentlyContinue | Out-Null
 
-(New-Object Net.WebClient).DownloadFile('https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Files/StoreApps/Microsoft.VCLibs.140.00_14.0.32530.0_x64__8wekyb3d8bbwe.Appx', "$env:temp\Microsoft.VCLibs_x64.Appx")
+(New-Object Net.WebClient).DownloadFile('https://s3.amazonaws.com/s3.bonben365.com/files/shared/StoreApps/Microsoft.VCLibs.140.00_14.0.32530.0_x64__8wekyb3d8bbwe.Appx', "$env:temp\Microsoft.VCLibs_x64.Appx")
 Add-AppxPackage -Path "$env:temp\Microsoft.VCLibs_x64.Appx" -ErrorAction SilentlyContinue | Out-Null
