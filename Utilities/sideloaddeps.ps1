2007152812f9b9baf8c4d36fb3332b41176c0428
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-07-01 20:42:37 +0700

    Update sideloaddeps.ps1

--- Utilities/sideloaddeps.ps1
+++ Utilities/sideloaddeps.ps1
@@ -20,8 +20,18 @@ Set-Location "$env:temp"
 # Installe Microsoft Store
 
     Invoke-WebRequest -Uri 'https://s3.amazonaws.com/s3.bonben365.com/files/shared/StoreApps/Microsoft.NET.Native.Runtime.1.6_1.6.24903.0_x64__8wekyb3d8bbwe.Appx' -OutFile 'Microsoft.NET.Native.Runtime.1.6_1.6.24903.0_x64__8wekyb3d8bbwe.Appx'
+    Invoke-WebRequest -Uri 'https://s3.amazonaws.com/s3.bonben365.com/files/shared/StoreApps/Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x64__8wekyb3d8bbwe.Appx' -OutFile 'Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x64__8wekyb3d8bbwe.Appx'
+
     Invoke-WebRequest -Uri 'https://s3.amazonaws.com/s3.bonben365.com/files/shared/StoreApps/Microsoft.NET.Native.Framework.1.6_1.6.24903.0_x64__8wekyb3d8bbwe.Appx' -OutFile 'Microsoft.NET.Native.Framework.1.6_1.6.24903.0_x64__8wekyb3d8bbwe.Appx'
+    Invoke-WebRequest -Uri 'https://s3.amazonaws.com/s3.bonben365.com/files/shared/StoreApps/Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x64__8wekyb3d8bbwe.Appx' -OutFile 'Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x64__8wekyb3d8bbwe.Appx'
+
 
     Add-AppxPackage 'Microsoft.NET.Native.Framework.1.6_1.6.24903.0_x64__8wekyb3d8bbwe.Appx'
+    Add-AppxPackage 'Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x64__8wekyb3d8bbwe.Appx'
+    
+    Add-AppxPackage 'Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x64__8wekyb3d8bbwe.Appx'
     Add-AppxPackage 'Microsoft.NET.Native.Runtime.1.6_1.6.24903.0_x64__8wekyb3d8bbwe.Appx'
 
+    
+
+Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x64__8wekyb3d8bbwe
