030b7d5f557bad0ad1675a711ff034dbd01b9ed5
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-07-26 15:02:32 +0700

    Update autounattend.ps1

--- Utilities/autounattend.ps1
+++ Utilities/autounattend.ps1
@@ -1,5 +1,5 @@
 
-
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v TargetReleaseversion /t REG_DWORD /d 1
 
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
 Start-Sleep -Seconds 1
