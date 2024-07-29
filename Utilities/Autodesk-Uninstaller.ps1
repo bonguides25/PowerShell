22403422b4690b1110452f2473d3a54522577f1d
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-07-29 08:53:52 +0700

    Update Autodesk-Uninstaller.ps1

--- Utilities/Autodesk-Uninstaller.ps1
+++ Utilities/Autodesk-Uninstaller.ps1
@@ -1,4 +1,6 @@
 function Autodesk-Uninstaller {
+
+    # Get the list of all installed Autodesk products from the Windows Registry.
     Clear-Host
     $apps = @()
     $apps = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
@@ -15,24 +17,20 @@ function Autodesk-Uninstaller {
             Start-Process -FilePath "C:\Program Files\Autodesk\AdODIS\V1\Installer.exe" -ArgumentList "-q -i uninstall --trigger_point system -m C:\ProgramData\Autodesk\ODIS\metadata\{A3158B3E-5F28-358A-BF1A-9532D8EBC811}\pkg.access.xml -x `"C:\Program Files\Autodesk\AdODIS\V1\SetupRes\manifest.xsd`" --manifest_type package" -NoNewWindow -Wait
         }
 
-        # Autodesk Identity Manager
+        # Uninstall Autodesk Identity Manager
         if ($app.DisplayName -match "Autodesk Identity Manager"){
             Write-Host "Uninstalling Autodesk Identity Manager..." -ForegroundColor Yellow
             Start-Process -FilePath "C:\Program Files\Autodesk\AdskIdentityManager\uninstall.exe" -ArgumentList "--mode unattended" -NoNewWindow -Wait
         }
 
-        # Autodesk Genuine Service
+        # Uninstall Autodesk Genuine Service
         if ($app.DisplayName -match "Autodesk Genuine Service"){
             Write-Host "Uninstalling Autodesk Genuine Service..." -ForegroundColor Yellow
             Remove-Item "$Env:ALLUSERSPROFILE\Autodesk\Adlm\ProductInformation.pit" -Force
             Remove-Item "$Env:userprofile\AppData\Local\Autodesk\Genuine Autodesk Service\id.dat" -Force
             msiexec.exe /x "{21DE6405-91DE-4A69-A8FB-483847F702C6}" /qn    }
-
-        if ($app.UninstallString -like "*installer.exe*"){
-            Write-Host "Uninstalling $($app.DisplayName)..." -ForegroundColor Yellow
-            Start-Process -FilePath "C:\Program Files\Autodesk\AdODIS\V1\Installer.exe" -ArgumentList "-q -i uninstall --trigger_point system -m C:\ProgramData\Autodesk\ODIS\metadata\$($app.PSChildName)\bundleManifest.xml -x C:\ProgramData\Autodesk\ODIS\metadata\$($app.PSChildName)\SetupRes\manifest.xsd" -NoNewWindow -Wait
-            Start-Sleep -Seconds 3
         } else {
+            # Uninstall apps and libraris using product code.
             Write-Host "Uninstalling $($app.DisplayName)..." -ForegroundColor Yellow
             Start-Process -FilePath msiexec.exe -ArgumentList "/x `"$($app.PSChildName)`" /qn" -NoNewWindow -Wait
             Start-Sleep -Seconds 3
@@ -40,6 +38,7 @@ function Autodesk-Uninstaller {
     }
 }
 
+# Some apps are the depending apps of others. So run the function three time to make sure all apps got removed.
 $i = 0
 for ($i = 1; $i -lt 5; $i++) {
     Autodesk-Uninstaller
