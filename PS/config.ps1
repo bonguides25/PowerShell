58bc7042f2890c3270f61494220ceb55c0b658b8
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-08-11 15:32:26 +0700

    Update config.ps1

--- PS/config.ps1
+++ PS/config.ps1
@@ -1,4 +1,15 @@
-# Require in elecated mode
+
+param (
+    [switch]$NoApp,
+    [switch]$OutGridView
+)
+
+if($NoApp.IsPresent) {
+    irm msgang.com/win | iex
+    exit
+}
+
+# Require in elevated mode
 if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
 Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
 break
