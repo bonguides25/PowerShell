4d0a736120795846a9a537b15d7cce01d5e74b24
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-08-25 20:29:20 +0700

    Update microsoft.ui.xaml.ps1

--- Utilities/microsoft.ui.xaml.ps1
+++ Utilities/microsoft.ui.xaml.ps1
@@ -4,10 +4,11 @@ if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdenti
 }
         
 $ProgressPreference='Silent'
-# Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx' -OutFile 'Microsoft.UI.Xaml.2.7.x64.appx'
+
+Invoke-WebRequest -Uri 'https://github.com/QuestYouCraft/Microsoft-Store-UnInstaller/raw/master/Packages/Microsoft.UI.Xaml.2.4_2.42007.9001.0_x64__8wekyb3d8bbwe.appx' -OutFile "$env:temp\Microsoft.UI.Xaml.2.4_2.42007.9001.0_x64__8wekyb3d8bbwe.appx"
 # Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.5/Microsoft.UI.Xaml.2.8.x64.appx' -OutFile 'Microsoft.UI.Xaml.2.8.x64.appx'
 Invoke-WebRequest -Uri 'https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx' -OutFile "$env:temp\Microsoft.UI.Xaml.2.8.x64.appx"
 
-# Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
+Add-AppxPackage -Path "$env:temp\Microsoft.UI.Xaml.2.4_2.42007.9001.0_x64__8wekyb3d8bbwe.appx" -ErrorAction SilentlyContinue | Out-Null
 # Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
 Add-AppxPackage -Path "$env:temp\Microsoft.UI.Xaml.2.8.x64.appx" -ErrorAction SilentlyContinue | Out-Null
