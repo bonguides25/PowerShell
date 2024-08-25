3db28716a64aa03b40199bbda1c3257c7a1a32b4
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-08-25 17:44:40 +0700

    Update msstore.ps1

--- Utilities/msstore.ps1
+++ Utilities/msstore.ps1
@@ -31,32 +31,3 @@ if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdenti
         exit
     }
 
-# Installing the Microsoft Store on Windows systems
-    Set-Location "$env:temp"
-    Write-Host "`nInstalling Microsoft Store..." -ForegroundColor Green
-    
-    # Install C++ Runtime framework packages for Desktop Bridge
-        $ProgressPreference='Silent'
-        irm https://raw.githubusercontent.com/bonguides25/PowerShell/main/Utilities/msvclibs.ps1 | iex
-    
-    # Install Microsoft.UI.Xaml
-        $ProgressPreference='Silent'
-        irm https://raw.githubusercontent.com/bonguides25/PowerShell/main/Utilities/microsoft.ui.xaml.ps1 | iex
-    
-    # Install the dependency packages
-        $ProgressPreference='Silent'
-        irm https://raw.githubusercontent.com/bonguides25/PowerShell/main/Utilities/sideloaddeps.ps1 | iex
-    
-    # Installe Microsoft Store
-        Invoke-WebRequest -Uri 'https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Files/StoreApps/Microsoft.WindowsStore_11809.1001.713.0_neutral_~_8wekyb3d8bbwe.AppxBundle' -OutFile 'Microsoft.WindowsStore_11809.1001.713.0_neutral_~_8wekyb3d8bbwe.AppxBundle'
-        Add-AppxPackage 'Microsoft.WindowsStore_11809.1001.713.0_neutral_~_8wekyb3d8bbwe.AppxBundle'
-    
-    # Downlaod and install Windows Package Manager to install Store apps
-        Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
-        Add-AppxPackage 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
-        
-    Get-AppxPackage | Where-Object { $_.name -like "*Store*" -or $_.name -like "*UI.Xaml*" -or $_.name -like "*DesktopAppInstaller*" } | Select-Object Name, Version -ErrorAction SilentlyContinue
-    Write-Host "Done.`n" -ForegroundColor Green
-    
-    # Open the Microsoft Store
-    start ms-windows-store:
