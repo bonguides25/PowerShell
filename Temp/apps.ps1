f3f5b0eb3e563cbdd1d8db8cfb99901be924b14e
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-11-23 20:24:07 +0700

    Create apps.ps1

---
+++ Temp/apps.ps1
@@ -0,0 +1,24 @@
+winget install Google.Chrome
+winget install Mozilla.Firefox
+winget install VideoLAN.VLC
+winget install mRemoteNG.mRemoteNG
+winget install WinSCP.WinSCP
+winget install OBSProject.OBSStudio
+winget install FastStone.Capture
+winget install EZBSystems.UltraISO
+winget install Brave.Brave
+
+
+winget install DucFabulous.UltraViewer
+winget install Telegram.TelegramDesktop
+winget install Microsoft.VisualStudioCode
+
+winget install Zoom.Zoom
+winget install Mozilla.Thunderbird
+winget install Foxit.FoxitReader
+winget install Evernote.Evernote
+winget install LIGHTNINGUK.ImgBurn
+winget install Piriform.CCleaner
+winget install Valve.Steam
+winget install BlenderFoundation.Blender
+winget install KDE.Krita
