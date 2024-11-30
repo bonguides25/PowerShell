1b781dafe31dfe26a3dcf05f0c828df6c006c9b6
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2024-11-30 17:41:31 +0700

    Update omp.ps1

--- Utilities/omp.ps1
+++ Utilities/omp.ps1
@@ -1,9 +1,13 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Start-Sleep -Seconds 1
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
 Start-Sleep -Seconds 1
 
 #Update the $env:Path to the current session
 $userpath = [System.Environment]::GetEnvironmentVariable("Path","User")
 $machinePath = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 $env:Path = $userpath + ";" + $machinePath
+
+Start-Sleep -Seconds 1
 
 oh-my-posh font install JetBrainsMono
