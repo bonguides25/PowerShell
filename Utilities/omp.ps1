d6be883b5fd3b0349ebce61435418c0b943ee51c
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2025-02-01 18:23:25 +0700

    Update omp.ps1

--- Utilities/omp.ps1
+++ Utilities/omp.ps1
@@ -1,9 +1,21 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 Start-Sleep -Seconds 1
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+$url = 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest'
+$request = [System.Net.WebRequest]::Create($url)
+$response = $request.GetResponse()
+$tagUrl = $response.ResponseUri.OriginalString
+$fileName = "install-x64.msi"
+
+# Create the download link
+$downloadUrl = $tagUrl.Replace('tag', 'download') + '/' + $fileName
+
+# Download to the temp folder
+(New-Object Net.WebClient).DownloadFile("$downloadUrl", "$env:TEMP/$fileName")
+
+msiexec.exe /i "$env:TEMP\install-x64.msi" /qn
 Start-Sleep -Seconds 1
 
-#Update the $env:Path to the current session
+# Update the $env:Path to the current session
 $userpath = [System.Environment]::GetEnvironmentVariable("Path","User")
 $machinePath = [System.Environment]::GetEnvironmentVariable("Path","Machine")
 $env:Path = $userpath + ";" + $machinePath
