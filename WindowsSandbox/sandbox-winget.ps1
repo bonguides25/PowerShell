ad86ea303625a8bfb63902b32601456aaa840cdd
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2025-02-01 17:53:56 +0700

    Update sandbox-winget.ps1

--- WindowsSandbox/sandbox-winget.ps1
+++ WindowsSandbox/sandbox-winget.ps1
@@ -16,24 +16,33 @@ if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdenti
     break
 }
 
-Write-Host "`nInstalling Windows Package Manager (winget)..." -ForegroundColor Yellow
+$progressPreference = 'silentlyContinue'
+Write-Host "Installing WinGet PowerShell module from PSGallery..." -ForegroundColor Yellow
+Install-PackageProvider -Name NuGet -Force | Out-Null
+Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..." -ForegroundColor Yellow
+Repair-WinGetPackageManager | Out-Null
+Write-Host "Winget version: $(winget -v)" -ForegroundColor Green
+Write-Host "Done."
+
+# Write-Host "`nInstalling Windows Package Manager (winget)..." -ForegroundColor Yellow
 
 
-# Install C++ Runtime framework packages for Desktop Bridge
-    $ProgressPreference='Silent'
-    irm https://raw.githubusercontent.com/bonguides25/PowerShell/main/Utilities/msvclibs.ps1 | iex
+# # Install C++ Runtime framework packages for Desktop Bridge
+#     $ProgressPreference='Silent'
+#     irm https://raw.githubusercontent.com/bonguides25/PowerShell/main/Utilities/msvclibs.ps1 | iex
 
-# Install Microsoft.UI.Xaml through Nuget.
-    $ProgressPreference='Silent'
-    irm https://raw.githubusercontent.com/bonguides25/PowerShell/main/Utilities/microsoft.ui.xaml.ps1 | iex
+# # Install Microsoft.UI.Xaml through Nuget.
+#     $ProgressPreference='Silent'
+#     irm https://raw.githubusercontent.com/bonguides25/PowerShell/main/Utilities/microsoft.ui.xaml.ps1 | iex
 
-# Install Windows Package Managet for install apps from Microsoft Store.
-    $progressPreference = 'silentlyContinue'
-    Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
-    Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+# # Install Windows Package Managet for install apps from Microsoft Store.
+#     $progressPreference = 'silentlyContinue'
+#     Invoke-WebRequest -Uri 'https://aka.ms/getwinget' -OutFile 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+#     Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 
-Write-Host "Winget version: $(winget -v)"
-winget
+# Write-Host "Winget version: $(winget -v)"
+# winget
 
 
 
