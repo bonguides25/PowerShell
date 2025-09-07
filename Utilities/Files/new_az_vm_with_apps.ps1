99c24404ca03aa933700921c86d6c16aa3bb5f50
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2025-09-07 16:14:37 +0700

    Update new_az_vm_with_apps.ps1

--- Utilities/Files/new_az_vm_with_apps.ps1
+++ Utilities/Files/new_az_vm_with_apps.ps1
@@ -8,13 +8,19 @@ Set-Location 'C:\ProgramData\chocolatey\bin'
 # .\choco.exe install oh-my-posh -y
 .\choco.exe install GoogleChrome -y --ignore-check
 .\choco.exe install firefox -y
-.\choco.exe install vlc -y
+# .\choco.exe install vlc -y
 # .\choco.exe install winscp -y
 # .\choco.exe install mremoteng -y
-.\choco.exe install zoom -y
+# .\choco.exe install zoom -y
 # .\choco.exe install discord -y
 # .\choco.exe install skype -y
 # .\choco.exe install thunderbird -y
 # .\choco.exe install fscapture -y
 # .\choco.exe install teamviewer -y
 # .\choco.exe install VisualStudioCode -y
+RefreshEnv
+[system.Diagnostics.Process]::Start("chrome","https://clip.opus.pro/tools/youtube-thumbnail-maker")
+Start-Sleep -Second 1
+[system.Diagnostics.Process]::Start("firefox","https://clip.opus.pro/tools/youtube-thumbnail-maker")
+Start-Sleep -Second 1
+[system.Diagnostics.Process]::Start("msedge","https://clip.opus.pro/tools/youtube-thumbnail-maker")
