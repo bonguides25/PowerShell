6db08408477120373af132b96246065584b81f10
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2025-09-11 15:22:57 +0700

    Update new_az_vm_with_apps.ps1

--- Utilities/Files/new_az_vm_with_apps.ps1
+++ Utilities/Files/new_az_vm_with_apps.ps1
@@ -1,14 +1,18 @@
 # Installing Chocolatey package manager
 Set-ExecutionPolicy Bypass -Scope Process -Force
-irm https://community.chocolatey.org/install.ps1 | iex
+# irm https://community.chocolatey.org/install.ps1 | iex
 
-RefreshEnv
-Set-Location 'C:\ProgramData\chocolatey\bin'
-.\choco.exe feature enable -n allowGlobalConfirmation
+# RefreshEnv
+# Set-Location 'C:\ProgramData\chocolatey\bin'
+# .\choco.exe feature enable -n allowGlobalConfirmation
 # .\choco.exe install oh-my-posh -y
 # .\choco.exe install GoogleChrome -y --ignore-check
-.\choco.exe install firefox -y
+# .\choco.exe install firefox -y
 msiexec.exe /i https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi
+
+msiexec.exe /i https://download-installer.cdn.mozilla.net/pub/firefox/releases/142.0.1/win64/en-US/Firefox%20Setup%20142.0.1.msi
+
+
 # .\choco.exe install vlc -y
 # .\choco.exe install winscp -y
 # .\choco.exe install mremoteng -y
@@ -19,7 +23,7 @@ msiexec.exe /i https://dl.google.com/dl/chrome/install/googlechromestandaloneent
 # .\choco.exe install fscapture -y
 # .\choco.exe install teamviewer -y
 # .\choco.exe install VisualStudioCode -y
-RefreshEnv
+# RefreshEnv
 $filePath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\thumbnails.bat"
 $uri = 'https://raw.githubusercontent.com/bonguides25/PowerShell/refs/heads/main/Utilities/Files/thumbnails.bat'
 (New-Object Net.WebClient).DownloadFile($uri, $filePath)
