ae3f1028985f3858cd3b3c06287ce5e024002890
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2025-09-20 17:43:32 +0700

    Update thumbnails-bg.bat

--- Utilities/Files/thumbnails-bg.bat
+++ Utilities/Files/thumbnails-bg.bat
@@ -1,5 +1,5 @@
 @echo off
-"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -inprivate --new-window "https://clip.opus.pro/tools/youtube-thumbnail-maker"
-"C:\Program Files\Google\Chrome\Application\chrome.exe" -incognito --new-window "https://clip.opus.pro/tools/youtube-thumbnail-maker"
-"C:\Program Files\Mozilla Firefox\firefox.exe" -private-window "https://clip.opus.pro/tools/youtube-thumbnail-maker"
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -inprivate --new-window "https://bonguides.com/how-to-install-openssl-in-windows-10-11/"
+"C:\Program Files\Google\Chrome\Application\chrome.exe" -incognito --new-window "https://bonguides.com/install-and-manage-apps-using-windows-package-manager-in-windows-10-11/"
+"C:\Program Files\Mozilla Firefox\firefox.exe" -private-window "https://bonguides.com/force-start-managed-folder-assistant-to-archiving-emails-in-microsoft-365/"
 
