b360dd6af50a9123329e5c44bb9ed6647eec7997
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2025-09-25 07:15:56 +0700

    Update thumbnails.bat

--- Utilities/Files/thumbnails.bat
+++ Utilities/Files/thumbnails.bat
@@ -1,7 +1,9 @@
 @echo off
 "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" -inprivate --new-window "https://clip.opus.pro/tools/youtube-thumbnail-maker"
 "C:\Program Files\Google\Chrome\Application\chrome.exe" -incognito --new-window "https://clip.opus.pro/tools/youtube-thumbnail-maker"
-"C:\Program Files\Mozilla Firefox\firefox.exe" -private-window "https://clip.opus.pro/tools/youtube-thumbnail-maker"
+rem "C:\Program Files\Mozilla Firefox\firefox.exe" -private-window "https://clip.opus.pro/tools/youtube-thumbnail-maker"
+"%localappdata%\BraveSoftware\Brave-Browser\Application\brave.exe" -incognito --new-window "https://clip.opus.pro/tools/youtube-thumbnail-maker"
+
 
 
 
