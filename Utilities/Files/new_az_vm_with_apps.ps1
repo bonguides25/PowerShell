4dbbea69a0d689ff5d0b1d9ac959dbe30fa2e2b6
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2025-09-25 07:17:12 +0700

    Update new_az_vm_with_apps.ps1

--- Utilities/Files/new_az_vm_with_apps.ps1
+++ Utilities/Files/new_az_vm_with_apps.ps1
@@ -10,7 +10,41 @@ Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 # .\choco.exe install GoogleChrome -y --ignore-check
 # .\choco.exe install firefox -y
 Start-Process -FilePath msiexec.exe -ArgumentList "/i https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi" -Wait
-msiexec.exe /i https://download-installer.cdn.mozilla.net/pub/firefox/releases/142.0.1/win64/en-US/Firefox%20Setup%20142.0.1.msi
+# msiexec.exe /i https://download-installer.cdn.mozilla.net/pub/firefox/releases/142.0.1/win64/en-US/Firefox%20Setup%20142.0.1.msi
+
+
+# GitHub API URL for the app manifest.
+$apiUrl = "https://api.github.com/repos/microsoft/winget-pkgs/contents/manifests/b/Brave/Brave"
+
+# Fetch version folders then filter only version folders.
+$versions = Invoke-RestMethod -Uri $apiUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+$versionFolders = $versions | Where-Object { $_.type -eq "dir" }
+
+# Extract and sort version numbers to get the latest version.
+$sortedVersions = $versionFolders | ForEach-Object { $_.name } | Sort-Object {[version]$_} -Descending -ErrorAction SilentlyContinue
+$latestVersion = $sortedVersions[0]
+
+# Get contents of the latest version folder to find the .installer.yaml file.
+$latestApiUrl = "$apiUrl/$latestVersion"
+$latestFiles = Invoke-RestMethod -Uri $latestApiUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+$installerFile = $latestFiles | Where-Object { $_.name -like "*.installer.yaml" }
+
+# Download and parse YAML content to get the Url of the latest installer file.
+$yamlUrl = $installerFile.download_url
+$yamlContent = Invoke-RestMethod -Uri $yamlUrl -Headers @{ 'User-Agent' = 'PowerShell' }
+$yamlString = $yamlContent -join "`n"
+$installerUrls = [regex]::Matches($yamlString, "InstallerUrl:\s+(http[^\s]+)") | ForEach-Object { $_.Groups[1].Value }
+$installerUrl = $installerUrls[0]
+
+# Download the latest installer then starting install or update the app if:
+# - The installed version is older than the latest version.
+# - The app is not installed ( $installedVersion = $null ).
+
+$webClient = [System.Net.WebClient]::new()
+$webClient.DownloadFile($installerUrl, "$env:TEMP\brave-latest.exe")
+
+# Start the install or update process.
+Start-Process -FilePath "$env:TEMP\brave-latest.exe" -Wait
 
 
 # .\choco.exe install vlc -y
