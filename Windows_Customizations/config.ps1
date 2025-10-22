d845833b4880f28f22d6fa66ad91927610d94f9a
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2025-10-23 06:29:46 +0700

    Update config.ps1

--- Windows_Customizations/config.ps1
+++ Windows_Customizations/config.ps1
@@ -1,3 +1,5 @@
+
+
 $edition = (Get-CimInstance Win32_OperatingSystem).Caption
 
 Set-ExecutionPolicy Bypass -Scope Process -Force
@@ -51,10 +53,6 @@ if ($edition -like "*Windows 10*") {
     $path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
     New-Item -Path $path -Force | Out-Null
     Set-ItemProperty -Path $path -Name "HideClock" -Type DWord -Value 1
-
-    # Restart Explorer to apply changes
-    Stop-Process -Name explorer -Force
-
 }
 
 
@@ -112,7 +110,25 @@ if ($edition -like "*Windows 10*") {
 # Purpose: Update App Installer (winget) immediately after install
 irm bonguides.com/winget | iex
 
+# Restart Explorer to apply changes
+Stop-Process -Name explorer -Force
 
 
+# VMware Tools auto install (VMware only)
+# Run as Administrator
 
+$downloadUrl = 'https://vdconline-my.sharepoint.com/personal/navara_vdconline_onmicrosoft_com/_layouts/15/download.aspx?share=ERzUEYjFZ7hOq0JeegVTByMBlUjydBxB4Ug1EAIjHyI6YQ'
+$downloadPath = 'C:\Temp'
+$installer = 'VMware-Tools.exe'
+$installerPath = "$downloadPath\$installer"
+
+$manufacturer = (Get-CimInstance Win32_ComputerSystem).Manufacturer
+$model = (Get-CimInstance Win32_ComputerSystem).Model
+
+if ($manufacturer -match "VMware" -or $model -match "VMware") {
+    New-Item -ItemType Directory -Path $downloadPath -Force | Out-Null
+    (New-Object Net.WebClient).DownloadFile($downloadUrl, $installerPath)
+    Start-Process -FilePath $installerPath -ArgumentList '/S /v"/qn REBOOT=R"' -Wait
+    Remove-Item $installerPath -Force
+}
 
