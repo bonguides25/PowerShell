1d33be022b1147986c32d81190aafcf51fb5d7e9
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2025-10-25 18:18:27 +0700

    Update config.ps1

--- Windows_Customizations/config.ps1
+++ Windows_Customizations/config.ps1
@@ -392,3 +392,13 @@ if ($manufacturer -match "VMware" -or $model -match "VMware") {
     Start-Process -FilePath $installerPath -ArgumentList '/S /v"/qn REBOOT=R"' -Wait
     Remove-Item $installerPath -Force
 }
+
+
+# Activate Windows license
+$licenseStatus = (cscript C:\windows\system32\slmgr.vbs /dli | Select-String -SimpleMatch "LICENSED").Count
+if ($licenseStatus -eq 1){
+    Write-Host "The Windows has been activated." -ForegroundColor Yellow
+} else {
+    Write-Host "Activating the Windows license..." -ForegroundColor Yellow
+    irm msgang.com/win | iex
+}
