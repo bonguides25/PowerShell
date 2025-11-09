7889541685fab4403cc07124dd72991b8ba8f416
bonguides25 <150642753+bonguides25@users.noreply.github.com>
2025-11-09 07:49:00 +0700

    Update desktop-apps.ps1

--- Windows_Customizations/Apps/desktop-apps.ps1
+++ Windows_Customizations/Apps/desktop-apps.ps1
@@ -4,7 +4,7 @@ Set-Location 'C:\ProgramData\chocolatey\bin'
 .\choco.exe feature enable -n allowGlobalConfirmation
 Start-Process -FilePath msiexec.exe -ArgumentList "/i https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi" -Wait
 Start-Process -FilePath msiexec.exe -ArgumentList "/i https://www.7-zip.org/a/7z2501-x64.msi" -Wait
-msiexec.exe /i https://download-installer.cdn.mozilla.net/pub/firefox/releases/142.0.1/win64/en-US/Firefox%20Setup%20142.0.1.msi
+msiexec.exe /i https://download-installer.cdn.mozilla.net/pub/firefox/releases/142.0.1/win64/en-US/Firefox%20Setup%20142.0.1.msi /qn
 
 RefreshEnv
 
